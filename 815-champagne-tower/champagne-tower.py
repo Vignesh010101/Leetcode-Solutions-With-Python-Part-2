22591c83ef0aa2c2948c4e797222cf8bbaec8a66
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-10 14:33:36 +0530

    Time: 94 ms (34.67%) | Memory: 19.8 MB (5.43%) - LeetSync

--- 815-champagne-tower/champagne-tower.py
+++ 815-champagne-tower/champagne-tower.py
@@ -1,31 +1,3 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        if poured==25 and query_row ==6 and query_glass==1:
-            return 0.1875
-        if poured==6 and query_row ==3 and (query_glass==1 or query_glass==2):
-            return 0.25
-        if poured==200 and query_row ==15 and query_glass==11:
-            return 1
-        dp = [[0 for _ in range(query_row+1)] for _ in range(query_row+1)]
-        k = poured
-        dp[0][0] = k
-        for i in range(1,query_row+1):
-            for j in range(i+1):
-                if j==0:
-                    dp[i][j] = (dp[i-1][j]-1)/2 
-                elif j==i:
-                    dp[i][j] = (dp[i-1][j-1]-1)/2 
-                else:
-                    dp[i][j] =((dp[i-1][j]-1)/2 ) + ((dp[i-1][j-1]-1)/2 )
-                # dp[i][j] = k 
-                print(i,j,dp[i][j])
-        if dp[query_row][query_glass] >1:
-            return 1
-        elif dp[query_row][query_glass] <=0:
-            return 0
-        else:
-            return dp[query_row][query_glass]
-                
-
-        
-        
+        return min((lambda func: lambda a, b, c: func(func, a, b, c))((functools.lru_cache(maxsize=None))(lambda champagneTower_, poured_, query_row_, query_glass_: poured_ if query_row_ == 0 else (max(champagneTower_(champagneTower_, poured_, query_row_-1, query_glass_-1) - 1, 0) / 2 if query_glass_ == query_row_ else (max(champagneTower_(champagneTower_, poured_, query_row_-1, query_glass_) - 1, 0) / 2 if query_glass_ == 0 else max(champagneTower_(champagneTower_, poured_, query_row_-1, query_glass_-1) - 1, 0) / 2 + max(champagneTower_(champagneTower_, poured_, query_row_-1, query_glass_) - 1, 0) / 2))))(poured, query_row, query_glass), 1.0)
