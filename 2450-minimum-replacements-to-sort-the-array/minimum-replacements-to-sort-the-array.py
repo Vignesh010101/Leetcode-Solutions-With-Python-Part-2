e4d1f76949e13a404410053252f54b3f7bd51f91
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-11 09:50:11 +0530

    Time: 428 ms (32.40%) | Memory: 28.2 MB (72.63%) - LeetSync

---
+++ 2450-minimum-replacements-to-sort-the-array/minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        N = len(nums)
+        splits = 0
+        
+        for idx in reversed(range(1, N)):
+            left, curr = nums[idx - 1], nums[idx]
+            
+            if left > curr:
+                quotient = left // curr
+                remainder = left % curr
+                
+                if remainder == 0:
+                    nums[idx - 1] = curr
+                    splits += quotient - 1
+                else:
+                    nums[idx - 1] = left // (quotient + 1)
+                    splits += quotient
+        
+        return splits
