80322f5704772595e736a785c330fca38fe2ff88
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-16 12:04:14 +0530

    Time: 1481 ms (88.72%) | Memory: 32.8 MB (56.39%) - LeetSync

---
+++ 2027-maximum-number-of-removable-characters/maximum-number-of-removable-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
+        def is_subsequence(k) -> bool:
+            # take the FIRST k elements from removable
+            remove_set = set(removable[:k])
+            pointer = 0
+            for i in range(len(s)):
+                if i in remove_set:
+                    continue
+                if s[i] == p[pointer]:
+                    pointer += 1
+                    if pointer == len(p):
+                        return True
+            return False
+        # binary search for k
+        res = 0
+        left, right = 1, len(removable)
+        while left <= right:
+            mid = (left + right)//2
+            if is_subsequence(mid):
+                res = max(res, mid)
+                left = mid + 1
+            else:
+                right = mid - 1
+        return res
