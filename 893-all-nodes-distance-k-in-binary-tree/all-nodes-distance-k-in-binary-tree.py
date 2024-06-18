9dcb27665ebf8ecf454e74827d59fdb0df78be62
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-18 19:10:10 +0530

    Time: 41 ms (44.89%) | Memory: 16.7 MB (84.69%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -5,61 +5,58 @@
 #         self.left = None
 #         self.right = None
 
-from collections import deque
-
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        if not root or not target:
-            return []
-        if k==0:
-            return [target.val]
-        
-        parent={root: None}
-        def dfs(root):
-            if not root:
-                return
-            if root.left:
-                parent[root.left]=root
-                dfs(root.left)
-            if root.right:
-                parent[root.right]=root
-                dfs(root.right)
-        dfs(root)
 
-        def bfs(root, level, visited):
-            if not root or level<0:
-                return []
-            if root in visited:
-                return []
-            pool=deque()
-            pool.append(root)
-            visited.add(root)
-            cnt=0
-            while pool and cnt<level:
-                tp=deque()
-                while pool:
-                    cur=pool.popleft()
-                    if cur.left and cur.left not in visited:
-                        tp.append(cur.left)
-                        visited.add(cur.left)
-                    if cur.right and cur.right not in visited:
-                        tp.append(cur.right)
-                        visited.add(cur.right)
-                pool=tp
-                cnt+=1
-            ans=[]
-            while pool:
-                cur=pool.popleft()
-                ans.append(cur.val)
-            return ans
-        
-        tp=target
-        ans=[]
-        visited=set()
-        for l in range(k+1):
-            ans.extend(bfs(tp, k-l, visited))
-            tp=parent[tp]
-            if not tp:
-                break
-        return ans
+        targetPath=""
+        nodePaths=[]
+        q=deque([(root, "")])
+
+        # Build Path for each node from root using Level order traversal
+        while(q):
+
+            node, path=q.popleft()
+
+            # Store the target path
+            if node==target:
+                targetPath=path
+
+            nodePaths.append((node.val, path))
+
+            # For left child add "l" in the path
+            if node.left:
+                q.append((node.left, path+"l"))
+            # For right child add "r" in the path
+            if node.right:
+                q.append((node.right, path+"r"))
+
+        # Find the length of path from root to target
+        tpLen=len(targetPath)
+        result=[]
+
+        # For each node path
+        for node, path in nodePaths:
+
+            p=0
+            tp=0
+            pLen=len(path)
+            
+            # Cover the path that is common between current node
+            # and the target node using two pointer
+            while(p<pLen and tp<tpLen):
+                
+                # Break when the path is no longer common
+                if path[p]!=targetPath[tp]:
+                    break
+
+                p+=1
+                tp+=1
+
+            # Distance= nodePath-commonPath + targetPath-commonPath
+            dist=pLen-p+tpLen-tp
+            if dist==k:
+                result.append(node)
+
+        return result
+
         
