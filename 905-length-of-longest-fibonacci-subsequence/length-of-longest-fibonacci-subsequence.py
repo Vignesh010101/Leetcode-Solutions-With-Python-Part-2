97fb149e33445d789b0bd573df7954b1a233a0da
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-18 19:16:56 +0530

    Time: 358 ms (97.01%) | Memory: 17.5 MB (34.33%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,18 +1,15 @@
 class Solution:
-    def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        nums = set(arr)
-        longest = 0
-
-        for i in range(len(arr)):
-            for j in range(i + 1, len(arr)):
-                a, b = arr[i], arr[j]
-                length = 2
-
-                while a + b in nums:
-                    a, b = b, a + b
-                    length += 1
-
-                if length > 2 and length > longest:
-                    longest = length
-
-        return longest
+  def lenLongestFibSubseq(self, arr: list[int]) -> int:
+    longest_subsequence = 0
+    index_from_value_table = {x: i for i, x in enumerate(arr)}
+    subsequence_length_dict = defaultdict(lambda: 2)
+    for k, k_val in enumerate(arr):
+      jlo = bisect_right(arr, k_val // 2, hi=k)
+      for j_val in arr[jlo:k]:
+        j = index_from_value_table[j_val]
+        i = index_from_value_table.get(k_val - j_val, None)
+        if i is None: continue
+        seq = subsequence_length_dict[i, j] + 1
+        subsequence_length_dict[j, k] = seq
+        longest_subsequence = max(longest_subsequence, seq)
+    return longest_subsequence
