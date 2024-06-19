0455b7c0fcbb3262465fe8d8f88e027cfb354d09
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-19 10:20:55 +0530

    Time: 602 ms (84.45%) | Memory: 23.8 MB (85.48%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        res = []
+        heapify(nums)
+        for _ in range(len(nums)):
+            res.append(heappop(nums))
+
+        return res
