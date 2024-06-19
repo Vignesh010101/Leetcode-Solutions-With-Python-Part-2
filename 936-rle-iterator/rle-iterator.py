b1888cf77e706ff66f6ad206ba80be6d20ce1d74
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-19 10:18:04 +0530

    Time: 49 ms (7.76%) | Memory: 17 MB (81.68%) - LeetSync

--- 936-rle-iterator/rle-iterator.py
+++ 936-rle-iterator/rle-iterator.py
@@ -1,31 +1,24 @@
 class RLEIterator:
 
     def __init__(self, encoding: List[int]):
-        self.qrle = deque()
-        n = len(encoding)
-        for i in range(0, n, 2):
-            count, elem = encoding[i], encoding[i+1]
-            if count:
-                self.qrle.append((count, elem))    
+        self.lst=encoding
 
     def next(self, n: int) -> int:
-        last_elem = 0
-        while n:
-            if not self.qrle:
-                last_elem = -1
-                break
-                
-            count, elem = self.qrle.popleft()
-            if count >= n:
-                last_elem = elem
-                if count > n: self.qrle.appendleft((count-n, elem))
-                break
+        num=-1
+        while self.lst and n>0:
+            if n<=self.lst[0]:
+                self.lst[0]-=n  
+                n=0
+                num=self.lst[1]
             else:
-                n -= count
-        
-        return last_elem
-
-
+                n-=self.lst[0]
+                self.lst[0]=0
+            if self.lst[0]==0:
+                self.lst.pop(0)
+                self.lst.pop(0)
+        if n>0:
+            return -1
+        return num
 # Your RLEIterator object will be instantiated and called as such:
 # obj = RLEIterator(encoding)
 # param_1 = obj.next(n)
