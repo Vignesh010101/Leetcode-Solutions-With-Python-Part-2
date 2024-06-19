382b99609967ac0d4cec0122713a8ae27fa1c8fa
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-19 10:29:16 +0530

    Time: 210 ms (31.13%) | Memory: 19.5 MB (33.38%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,6 +1,18 @@
-from itertools import accumulate
-from functools import reduce
-
 class Solution:
+       
     def minFlipsMonoIncr(self, s: str) -> int:
-        return reduce(lambda acc, curr: acc if int(curr[0]) else min(curr[1], acc + 1), zip(s, accumulate(map(int, s))), 0)
+        n=len(s)
+        ones=[0]*(n)
+        ct=0
+        for i in range(n):
+            if s[i]=='1':
+                ct+=1
+            ones[i]=ct
+        prev=0
+        for i in range(1,n+1):
+            if s[i-1]=='0':
+                curr=min(prev+1,ones[i-1])
+            else:
+                curr=min(prev,ones[i-1])
+            prev=curr
+        return prev
