a81ec486ff83232209edfdf568bdc808fa80eceb
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-19 10:15:51 +0530

    Time: 125 ms (29.61%) | Memory: 19.7 MB (93.54%) - LeetSync

--- 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -1,36 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from copy import deepcopy
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from functools import lru_cache
 class Solution:
     def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
-        ans = []
-        if not n % 2: # impossible to produce even num full tree
-            return ans
-        # combinatorial search with backtracking, at each step either set two children or set 0 children
-        def generate_trees(n, node, unprocessed):
-            if n == 0:
-                ans.append(deepcopy(head))
-                return
-            node.left = TreeNode()
-            node.right = TreeNode()
-            unprocessed.append(node.left)
-            unprocessed.append(node.right)
-            # go over every combination of dfs expansion based on unprocessed candidates
-            for ind, next_node in enumerate(unprocessed):
-                generate_trees(n - 2, next_node, unprocessed[ind+1:])
-                next_node.left = next_node.right = None # backtrack
-                if n - 2 == 0: return # don't double count dups
-        head = TreeNode()
-        unprocessed = []
-        generate_trees(n-1, head, unprocessed)
-        return ans  
+        @lru_cache(None)
+        def recurse(count):
+            if count == 1: return [TreeNode()]
+            output = []
+            for i in range((count - 1) // 2):
+                l_output, r_output = recurse(i * 2 + 1), recurse(count - 2 - i * 2)
+                for j in range(len(l_output)):
+                    for k in range(len(r_output)):
+                        output.append(TreeNode(val=0, left=l_output[j], right=r_output[k]))
+            return output
+        return recurse(n)
