217b132a4f40db6b0dd7ac346f92c5b99bd5b18a
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-19 10:30:47 +0530

    Time: 421 ms (11.66%) | Memory: 17.5 MB (81.05%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.py
+++ 971-shortest-bridge/shortest-bridge.py
@@ -1,58 +1,48 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
- 
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]  # right, left, up, down
-        
-        n = len(grid)
-        m = len(grid[0])
-        
-        # find one point of a land
-        def one_land(grid):
-            for in_x, row in enumerate(grid):
-                for in_y, val in enumerate(row):
-                    if val:
-                        return (in_x, in_y)
-                    
-        # start from one point and get all position of a land
-        in_x, in_y = one_land(grid)
-        queue = [(in_x, in_y)]
-        first_land = [(in_x, in_y)]     # temp queue to find the first land
-        
-        from collections import defaultdict
-        visited = defaultdict(lambda:False, {})
-        visited[(in_x, in_y)] = True        
-        
-        # find positions of the first land
-        while first_land:
-            x, y = first_land.pop(0)
-            for dir_x, dir_y in directions:
-                nei_x = x + dir_x
-                nei_y = y + dir_y
-                if nei_x in range(n) and nei_y in range(m):       # point in range
-                    if grid[nei_x][nei_y]:   # it is land
-                        if not visited[(nei_x, nei_y)]:
-                            first_land.append((nei_x, nei_y))
-                            queue.append((nei_x, nei_y))
-                            visited[(nei_x, nei_y)] = True
-        
-       
-        
-        # BFS
-        cost = 0
-        layer_size = 0
-        while queue:
-            layer_size = len(queue)
-            for i in range(layer_size):
-                x, y = queue.pop(0)
-                for dir_x, dir_y in directions:
-                    nei_x = x + dir_x
-                    nei_y = y + dir_y
-                    if nei_x in range(n) and nei_y in range(m):       # point in range
-                        if not visited[(nei_x, nei_y)]:
-                            if grid[nei_x][nei_y]:   # it is land (second land)
-                                return cost
-                            else:           # it is water
-                                queue.append((nei_x, nei_y))
-                                visited[(nei_x, nei_y)] = True
-            cost += 1
-        return None      
+        N = len(grid)
+
+        def get_neighbors(i, j):
+            for ni, nj in ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)):
+                if 0 <= ni < N and 0 <= nj < N:
+                    yield ni, nj
+
+        # Find a piece of an island
+        start = None
+        for i in range(N):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    start = (i, j)
+                    break
+            if start:
+                break
+
+        # BFS to find perimeter around one island
+        q = deque([start])
+        seen = set()
+        water = set()
+        while q:
+            i, j = q.popleft()
+            if (i, j) in seen:
+                continue
+            seen.add((i, j))
+            for ni, nj in get_neighbors(i, j):
+                if grid[ni][nj] == 0:
+                    water.add((ni, nj))
+                else:
+                    q.append((ni, nj))
+
+        # BFS from the perimeter out, until the other island is reached
+        q = deque(water)
+        res = 0
+        while q:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                if grid[i][j] == 1:
+                    return res
+                for ni, nj in get_neighbors(i, j):
+                    if (ni, nj) in seen:
+                        continue
+                    seen.add((ni, nj))
+                    q.append((ni, nj))
+            res += 1
