4e8f81612bcbc95e06d41166b9fea2647c610d08
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-20 20:53:42 +0530

    Time: 212 ms (7.27%) | Memory: 25 MB (12.73%) - LeetSync

--- 2701-subsequence-with-the-minimum-score/subsequence-with-the-minimum-score.py
+++ 2701-subsequence-with-the-minimum-score/subsequence-with-the-minimum-score.py
@@ -1,19 +1,27 @@
 class Solution:
     def minimumScore(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        left, right = [-1] * (n+2), [m] * (n+2)
+        p = 0
+        for i in range(n):
+            while p < m and s[p] != t[i]: p += 1
+            left[i+1] = p
+            p += 1
+        p = m - 1
+        for i in range(n-1, -1, -1):
+            while p >= 0 and s[p] != t[i]: p -= 1
+            right[i+1] = p
+            p -= 1
 
-        m, n =  len(s), len(t)
-        suff, j = [0]*m, n
+        def check(x): 
+            for i in range(n + 1 - x):
+                if left[i] < right[i+x+1]:
+                    return True
+            return False
 
-        for i in range(m)[::-1]:
-
-            if j > 0 and s[i] == t[j-1]: j -= 1
-            suff[i] = j-1
-
-        ans, j = j, 0
-
-        for i in range(m):
-
-            ans = min(ans, max(0, suff[i] - j + 1))
-            if j < n and s[i] == t[j]: j += 1
-
-        return min(ans, n-j)
+        l, r = 0, n
+        while l <= r:
+            x = (l + r) // 2;
+            if (check(x)): r = x - 1
+            else: l = x + 1
+        return l
