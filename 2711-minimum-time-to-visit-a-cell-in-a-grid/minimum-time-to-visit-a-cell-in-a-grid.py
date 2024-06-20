eff2f5e5bf2c374420a4a641121fc53f8f4c7bee
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-20 20:56:04 +0530

    Time: 1760 ms (58.64%) | Memory: 33.3 MB (72.84%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -1,33 +1,22 @@
 class Solution:
     def minimumTime(self, grid: List[List[int]]) -> int:
-        R, C = 0, 1
-        nr, nc = len(grid), len(grid[0])
-
-        def get_nx(r, c):
-            for r_nx, c_nx in ((r+1,c),(r-1,c),(r,c+1),(r,c-1)):
-                if 0 <= r_nx <= nr-1 and 0 <= c_nx <= nc-1:
-                    yield r_nx, c_nx
-
-        # main
+        m, n = len(grid), len(grid[0])
         if grid[0][1] > 1 and grid[1][0] > 1:
             return -1
-        start, end = (0, 0), (nr-1, nc-1)
-        t_to_cells = defaultdict(set)
-        t = 0
-        t_to_cells[t].add(start)
-        visited = {start}
-        while True:
-            for r, c in t_to_cells[t]:
-                for r_nx, c_nx in get_nx(r, c):
-                    if (r_nx, c_nx) in visited:
-                        continue
-                    if grid[r_nx][c_nx] > t+1:
-                        t_diff = grid[r_nx][c_nx] - t
-                        t_nx = t + t_diff if t_diff % 2 == 1 else t + t_diff + 1
-                    else:
-                        t_nx = t+1
-                    if (r_nx, c_nx) == end:
-                        return t_nx
-                    t_to_cells[t_nx].add((r_nx, c_nx))
-                    visited.add((r_nx, c_nx))
-            t += 1
+        visited = [[False] * n for _ in range(m)]
+        heap = [(0, 0, 0)] # (t, r, c)
+        while heap:
+            t, r, c = heapq.heappop(heap)
+            if r == m - 1 and c == n - 1:
+                return t
+            if visited[r][c]:
+                continue
+            visited[r][c] = True
+            for dr, dc in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nr >= m or nc < 0 or nc >= n or visited[nr][nc]:
+                    continue
+                wait = (grid[nr][nc] - t) % 2 == 0
+                nt = max(grid[nr][nc] + wait, t + 1)
+                heapq.heappush(heap, (nt, nr, nc))
+        return -1
