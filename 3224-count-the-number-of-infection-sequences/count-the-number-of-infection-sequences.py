2cb714169d8e3467dc937c0531e734e0841a5dba
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-23 19:12:11 +0530

    Time: 218 ms (41.07%) | Memory: 98.3 MB (41.07%) - LeetSync

---
+++ 3224-count-the-number-of-infection-sequences/count-the-number-of-infection-sequences.py
@@ -0,0 +1,30 @@
+class Solution:
+    MOD = 10 ** 9 + 7
+
+    @staticmethod
+    @cache
+    def factorial(n: int) -> int:
+        if n == 0:
+            return 1
+        return n * Solution.factorial(n - 1) % Solution.MOD
+
+    def numberOfSequence(self, n: int, sick: List[int]) -> int:
+        # DD, DDD, DDDD
+        # 9! / 2! / 3! / 4! * (1) * (2 * 2 * 1) * (1)
+        lengths = [sick[0], n - sick[-1] - 1]
+        for a, b in pairwise(sick):
+            length = b - a - 1
+            if length > 0:
+                lengths.append(length)
+
+        res = Solution.factorial(sum(lengths))
+        for i, x in enumerate(lengths):
+            res *= pow(Solution.factorial(x), -1, Solution.MOD)
+            res %= Solution.MOD
+
+            # do not multiply by 2 for beginning and end
+            if i >= 2:
+                res *= pow(2, x - 1, Solution.MOD)
+                res %= Solution.MOD
+
+        return res
