b72725e3b078a0615f1b9259512323cf6724ad95
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-27 18:34:38 +0530

    Time: 164 ms (91.61%) | Memory: 17.9 MB (64.34%) - LeetSync

--- 1139-previous-permutation-with-one-swap/previous-permutation-with-one-swap.py
+++ 1139-previous-permutation-with-one-swap/previous-permutation-with-one-swap.py
@@ -1,18 +1,10 @@
 class Solution:
-    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:    
         n = len(arr)
-        for i in range(n-1, 0, -1):
-            if arr[i-1] > arr[i]:
-                break
-        maxElement = arr[i]
-        maxIndex = i
-
-        for j in range(i, n):
-            print("j = ",j)
-            if (maxElement < arr[j]) and (arr[j] < arr[i-1]):
-                maxElement = arr[j]
-                maxIndex = j
-        #         print("pos = ",maxIndex)
-        #         print("Max = ", maxElement)
-        arr[i-1], arr[maxIndex] = arr[maxIndex], arr[i-1]
+        for i in range(n-2,-1,-1):
+            if arr[i] > arr[i+1]:
+                for j in range(n-1,i,-1):
+                    if arr[j] < arr[i] and (j == i-1 or arr[j] != arr[j-1]):
+                        arr[i],arr[j] = arr[j],arr[i]
+                        return arr
         return arr
