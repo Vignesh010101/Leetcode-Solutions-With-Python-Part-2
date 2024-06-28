49867c3d6cb12adefe54ff1e62a60a3d6af90e9e
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-06-28 16:16:14 +0530

    Time: 62 ms (75.28%) | Memory: 17.8 MB (74.91%) - LeetSync

---
+++ 1157-insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sufficientSubset(self, root: TreeNode, limit: int) -> TreeNode:
+        def dfs(node, path_sum):
+            if not node:
+                return False
+            path_sum += node.val
+            if not node.left and not node.right:
+                return path_sum >= limit
+            left = dfs(node.left, path_sum)
+            right = dfs(node.right, path_sum)
+            if not left:
+                node.left = None
+            if not right:
+                node.right = None
+            return left or right
+        result = dfs(root, 0)
+        return root if result else None      
