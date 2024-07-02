e2c36ed39f063a618dfa168dea7d5122dbcfad97
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-02 21:57:36 +0530

    Time: 51 ms (94.51%) | Memory: 16.7 MB (87.31%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.py
+++ 1187-print-foobar-alternately/print-foobar-alternately.py
@@ -2,18 +2,22 @@ from threading import Lock
 class FooBar:
     def __init__(self, n):
         self.n = n
-        self.l1 = Lock() # Semaphore(1)
-        self.l2 = Lock() # Semaphore(1)
-        self.l2.acquire()
+        self.foo_lock = Lock()
+        self.bar_lock = Lock()
+        self.bar_lock.acquire()
+
 
     def foo(self, printFoo: 'Callable[[], None]') -> None:
         for i in range(self.n):
-            self.l1.acquire()
+            self.foo_lock.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
             printFoo()
-            self.l2.release()
+            self.bar_lock.release()
+
 
     def bar(self, printBar: 'Callable[[], None]') -> None:
         for i in range(self.n):
-            self.l2.acquire()
+            self.bar_lock.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
             printBar()
-            self.l1.release()
+            self.foo_lock.release()
