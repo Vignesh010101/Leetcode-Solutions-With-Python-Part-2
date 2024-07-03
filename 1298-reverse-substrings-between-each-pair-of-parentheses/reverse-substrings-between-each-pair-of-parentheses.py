12ca724d57b11c1c1f71b7268e9c4c3bad5cf5f0
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-03 13:31:10 +0530

    Time: 39 ms (30.21%) | Memory: 16.5 MB (83.31%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,23 +1,13 @@
 class Solution:
     def reverseParentheses(self, s: str) -> str:
-        def solve(string):
-            n = len(string)
-            word = ""
-            i = 0
-            while i <n:
-                if string[i] == '(':
-                    new = ""
-                    count = 0
-                    while True:
-                        count += 1 if string[i] == '(' else -1 if string[i] == ')' else 0
-                        if count == 0: break
-                        new += string[i]
-                        i += 1
-                    i += 1
-                    word += solve(new[1:])
-                else:
-                    word += string[i]
-                    i += 1
-            return word[::-1]
-        return solve(s)[::-1]
-    
+        stack = []
+        for i in s:
+            if i == ')':
+                tmp = ""
+                while stack[-1] != '(':
+                    tmp += stack.pop()
+                stack.pop()
+                for j in tmp: stack.append(j)
+            else:
+                stack.append(i)
+        return "".join(stack)
