e5b923d343902d198df108e3415fdbc7652afa38
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-08 17:42:03 +0530

    Time: 31 ms (90.69%) | Memory: 16.5 MB (84.30%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,11 +1,8 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        circle: list[int] = [num for num in range(1, n + 1, 1)]
-        cur_ind: int = 0
+        def recursion(n, k):
+            if n == 1:
+                return 0
+            return (recursion(n - 1, k) + k) % n
 
-        while len(circle) != 1:
-            next_to_remove: int = (cur_ind + k - 1) % len(circle)
-            circle.pop(next_to_remove)
-            cur_ind = next_to_remove
-
-        return circle[0]
+        return recursion(n, k) + 1
