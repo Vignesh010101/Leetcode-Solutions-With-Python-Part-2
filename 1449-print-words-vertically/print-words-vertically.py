a2afa33c7fb67f20013a419251261f880e96d659
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-08 18:16:07 +0530

    Time: 42 ms (9.68%) | Memory: 16.5 MB (51.94%) - LeetSync

--- 1449-print-words-vertically/print-words-vertically.py
+++ 1449-print-words-vertically/print-words-vertically.py
@@ -1,14 +1,7 @@
+from itertools import zip_longest
+from typing import Iterable
+
+
 class Solution:
-    def printVertically(self, s: str) -> List[str]:
-        words = s.split()
-        max_len = max(len(word) for word in words)
-        res = []
-        for i in range(max_len):
-            vert_words = ''
-            for word in words:
-                if i < len(word):
-                    vert_words += word[i]
-                else:
-                    vert_words += ' '
-            res.append(vert_words.rstrip())
-        return res
+    def printVertically(self, s: str) -> Iterable[str]:
+        return map(str.rstrip, map(''.join, zip_longest(*s.split(), fillvalue=' ')))
