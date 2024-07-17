c81725492385235723f7e1d5de0529820312c8e4
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-17 17:51:56 +0530

    Time: 1173 ms (61.66%) | Memory: 27.7 MB (27.98%) - LeetSync

--- 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.py
+++ 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.py
@@ -1,12 +1,24 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        s=Counter()
-        res=0
-        for n in nums:
-            ns=Counter()
-            for ss,v in s.items():
-                ns[ss & n]+=v
-            ns[n]+=1
-            res+=ns[k]
-            s=ns
-        return res
+        prefix_and_counts = defaultdict(int)
+        total_count = 0
+        
+        for num in nums:
+            current_and_counts = defaultdict(int)
+            
+            for prefix_and, count in prefix_and_counts.items():
+                and_result = prefix_and & num
+                current_and_counts[and_result] += prefix_and_counts[prefix_and]
+            
+            current_and_counts[num] += 1
+            
+            for result, count in current_and_counts.items():
+                if result == k:
+                    total_count += count
+            
+            prefix_and_counts = current_and_counts
+        
+        return total_count
