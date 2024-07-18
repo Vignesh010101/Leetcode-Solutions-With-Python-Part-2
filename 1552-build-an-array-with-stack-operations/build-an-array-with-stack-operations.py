dee6996bd9c33eb40949c4fa516e33b825ba4a94
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-18 16:26:47 +0530

    Time: 38 ms (49.28%) | Memory: 16.5 MB (69.18%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        vy=[]
+        sc=[]
+        for i in range(1,n+1):
+            if i in target:
+                sc.append(i)
+                vy.append("Push")
+                if sc==target:
+                    return vy
+                    break
+            else:
+                vy.append("Push")
+                vy.append("Pop")
+        return vy        
