b46f818ad7f7425c978dbb71dff523105bbe85f5
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-18 16:32:57 +0530

    Time: 41 ms (24.72%) | Memory: 16.3 MB (96.47%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -1,16 +1,17 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        list1=[x for x in range(1,n+1)]
-        list2=[]
-        list3=[]
-        for i in range(len(list1)):
-            list2.append("Push")
-            list3.append(list1[i])
-            if(list1[i] not in target):
-                list2.append("Pop")
-                list3.pop()
-            elif(list3==target):
-                break
-        return list2
+        vy=[]
+        sc=[]
+        for i in range(1,n+1):
+            if i in target:
+                sc.append(i)
+                vy.append("Push")
+                if sc==target:
+                    return vy
+                    break
 
-        
+            else:
+                vy.append("Push")
+                vy.append("Pop")
+
+        return vy       
