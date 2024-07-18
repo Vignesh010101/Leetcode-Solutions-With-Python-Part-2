703c05983dd43382c4f9b163c600f651e0a2c1a3
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-18 13:33:39 +0530

    Time: 1264 ms (5.05%) | Memory: 21.7 MB (6.77%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
@@ -4,25 +4,31 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
-        def dfs(node):
-            if not node:
-                return []
-            if not node.left and not node.right:
-                return [1]  
+        self.map = {}
+        self.leaves = []
+        self.findLeaves(root, [], self.leaves, self.map)
+        res = 0
+        for i in range(len(self.leaves)):
+            for j in range(i + 1, len(self.leaves)):
+                list_i, list_j = self.map[self.leaves[i]], self.map[self.leaves[j]]
+                for k in range(min(len(list_i), len(list_j))):
+                    if list_i[k] != list_j[k]:
+                        dist = len(list_i) - k + len(list_j) - k
+                        if dist <= distance:
+                            res += 1
+                        break
+        return res
 
-            left_distances = dfs(node.left)
-            right_distances = dfs(node.right)
-            
-            for l in left_distances:
-                for r in right_distances:
-                    if l + r <= distance:
-                        self.count += 1
-            
-            return [d + 1 for d in left_distances + right_distances if d + 1 <= distance]
-
-        self.count = 0
-        dfs(root)
-        return self.count
+    def findLeaves(self, node: TreeNode, trail: List[TreeNode], leaves: List[TreeNode], map: Dict[TreeNode, List[TreeNode]]):
+        if not node:
+            return
+        tmp = trail[:]
+        tmp.append(node)
+        if not node.left and not node.right:
+            map[node] = tmp
+            leaves.append(node)
+            return
+        self.findLeaves(node.left, tmp, leaves, map)
+        self.findLeaves(node.right, tmp, leaves, map)                
