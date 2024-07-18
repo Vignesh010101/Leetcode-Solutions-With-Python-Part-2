567416c21c36fefc441b70c4c1b900ff111f34e7
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-18 16:34:36 +0530

    Time: 515 ms (82.89%) | Memory: 51.8 MB (93.38%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,17 +1,27 @@
 class Solution:
+    def dfs(self,node,grid,hasApple,visited):
+        visited[node]=1
+        ct=0
+        for i in grid[node]:
+            if visited[i]==1:
+                continue
+            x=self.dfs(i,grid,hasApple,visited)
+            if x!=-1:
+                ct+=x
+                ct+=2
+        if ct>0 or hasApple[node]==True:
+            return ct
+        return -1
+
+
+
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
-        def dfs(node, parent):
-            steps = 0
-            for child in adj[node]:
-                if child != parent: 
-                    steps += dfs(child, node)
-            
-            return steps + 2 if steps or hasApple[node] else 0
-        
-        adj = defaultdict(list)
-        for a, b in edges:
-            adj[a].append(b)
-            adj[b].append(a)
-            
-        result = dfs(0, None)        
-        return result - 2 if result else 0
+        grid=[[] for i in range(n)]
+        for i, j in edges:
+            grid[i].append(j)
+            grid[j].append(i)
+        visited=[0]*n
+        x=self.dfs(0,grid,hasApple,visited)
+        if x==-1:
+            return 0
+        return x
