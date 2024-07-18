2b59b9b6fdcdd76d74e922dcb02326af66b86b28
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-18 16:18:06 +0530

    Time: 121 ms (90.73%) | Memory: 18.9 MB (72.85%) - LeetSync

---
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkIfCanBreak(self, s1: str, s2: str) -> bool:
+        s1=sorted(s1)
+        s2=sorted(s2)
+        mi=min(s1,s2)
+        ma=max(s1,s2)
+        for i in range(len(s1)):
+            if mi[i]<=ma[i]:
+                continue
+            else:
+                return False
+                break
+        else:
+            return True
