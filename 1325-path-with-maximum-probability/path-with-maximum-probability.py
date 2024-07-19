a3cfd106cb5d97080b8ce387a99203b64e625b81
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-19 12:56:04 +0530

    Time: 537 ms (75.04%) | Memory: 28.6 MB (46.47%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -1,30 +1,27 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        def build_graph(n, edges, probability):
-            graph = {}
-            for i in range(n):
-                graph[i] = []
-            for i in range(len(edges)):
-                frm, to = edges[i]
-                graph[frm].append((probability[i], to))
-                graph[to].append((probability[i], frm))
-            return graph
-        def bfs(graph):
-            import heapq as hq
-            dist = {start:-1}
-            heap = [(-1, start)]
-            hq.heapify(heap)
-            while len(heap) > 0:
-                prob, node = hq.heappop(heap)
-                for child in graph[node]:
-                    p,c = child
-                    if c in dist:
-                        if abs(p*prob) > abs(dist[c]):
-                            hq.heappush(heap, (-abs(p*prob), c))
-                            dist[c] = -abs(p*prob)
-                    else:
-                        hq.heappush(heap, (-abs(p * prob), c))
-                        dist[c] = -abs(p * prob)
-            return 0 if end not in dist else -dist[end]
+        adj = [[] for _ in range(n)] 
+        for i, (a, b) in enumerate(edges):
+            adj[a].append((b, succProb[i]))  
+            adj[b].append((a, succProb[i]))  
+        probabilities = [0] * n  
+        probabilities[start] = 1  
+        pq = []  
+        heapq.heappush(pq, (-1, start))  
+        while pq:
+            probability, node = heapq.heappop(pq)  
+            probability *= -1  
 
-        return bfs(build_graph(n, edges, succProb))
+            if node == end:  
+                return probability
+
+            if probability < probabilities[node]:  
+                continue
+
+            for neighbor, edge_prob in adj[node]:  
+                new_prob = probability * edge_prob  
+                if new_prob > probabilities[neighbor]:  
+                    probabilities[neighbor] = new_prob  
+                    heapq.heappush(pq, (-new_prob, neighbor))  
+
+        return 0
