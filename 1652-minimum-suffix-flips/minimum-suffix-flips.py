aef457dbdb358522525ab838976487b52d7a1109
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-19 13:14:22 +0530

    Time: 51 ms (82.87%) | Memory: 17.2 MB (27.68%) - LeetSync

--- 1652-minimum-suffix-flips/minimum-suffix-flips.py
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.py
@@ -1,14 +1,9 @@
-class Solution(object):
-    def minFlips(self, target):
-        if '1' not in target:
-            return 0
-        ok = '0'
-        ans, i, n = 0, target.find('1'), len(target)
-        while i<n:
-            if ok != target[i]:
-                ans += 1
-                ok = target[i]
-            i += 1
-        return ans
-
-        
+class Solution:
+    def minFlips(self, target: str) -> int:
+        count = 0
+        last = '0'
+        for b in target:
+            if last != b:
+                count += 1
+                last = b
+        return count
