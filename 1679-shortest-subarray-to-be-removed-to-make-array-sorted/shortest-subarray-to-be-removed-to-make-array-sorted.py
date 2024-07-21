09215dd25671809dae6a5cb769cf8a22b9f6dd09
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-21 16:40:50 +0530

    Time: 464 ms (76.26%) | Memory: 31.2 MB (6.85%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,18 +1,29 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        arr.append(float("inf"))
-        arr.insert(0, 0)
+
+        n = len(arr)
+        s = 1
+        ans1 = 1
+        start = [arr[0]]
+        while s < n and arr[s-1] <= arr[s]:
+            ans1 += 1
+            start.append(arr[s])
+            s += 1
+        if start == arr:
+            return 0
         
-        left = 0
-        right = len(arr) - 1
-        shortest = float("inf")
-        while left < len(arr) - 2 and arr[left] <= arr[left + 1]:
-            left += 1
-        while left >= 0:
-            while right - 1 > left and arr[right - 1] >= arr[left] and arr[right] >= arr[right - 1]:
-                right -= 1
-            shortest = min(shortest, right - left - 1)
-            left -= 1
-            
-        return shortest
+        e = n-2
+        ans2 = 1
+        end = [arr[n-1]]
+        while e > 0 and arr[e] <= arr[e+1]:
+            ans2 += 1
+            end.append(arr[e])
+            e -= 1
         
+        ans = max(ans1, ans2)
+
+        m = len(end)
+        for j in range(m-1, -1, -1):
+            ans = max(ans, bisect.bisect_right(start, end[j]) + j + 1)
+        
+        return n-ans
