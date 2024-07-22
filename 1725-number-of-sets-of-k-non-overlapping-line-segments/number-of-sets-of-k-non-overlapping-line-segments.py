fc7db8c719a10b1e3b5a8881143fe5151984bad4
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-22 11:40:49 +0530

    Time: 2808 ms (6.67%) | Memory: 765.4 MB (6.67%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.py
@@ -1,14 +1,32 @@
+import sys
+sys.setrecursionlimit(10**9)
+
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        MOD = 10**9 + 7
+        M = 10**9+7
+        
         @lru_cache(None)
-        def dp(i, k, isStart):
-            if k == 0: return 1
-            if i == n: return 0 
-            ans = dp(i+1, k, isStart) 
-            if isStart:
-                ans += dp(i+1, k, False) 
+        def helper(idx, rem, isInMiddle):
+            if idx==n-1:
+                if rem:
+                    return 0
+                else:
+                    return 1
+            ans = 0
+            if isInMiddle:
+                ans += helper(idx+1, rem, isInMiddle)
+                ans %= M
+                ans += helper(idx+1, rem, not isInMiddle)
+                ans %= M
+                if rem>0:
+                    ans += helper(idx+1, rem-1, isInMiddle)
+                    ans %= M
             else:
-                ans += dp(i, k-1, True)
-            return ans % MOD
-        return dp(0, k, True)
+                ans += helper(idx+1, rem, isInMiddle)
+                ans %= M
+                if rem>0:
+                    ans += helper(idx+1, rem-1, not isInMiddle)
+                    ans %= M
+            return ans
+        return helper(0, k, False)
+    
