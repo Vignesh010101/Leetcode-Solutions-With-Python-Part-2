53f6991e8c8d5b06057a8f49e4cf66125623f571
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-22 19:22:22 +0530

    Time: 984 ms (94.89%) | Memory: 16.7 MB (97.81%) - LeetSync

---
+++ 1808-stone-game-vii/stone-game-vii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stoneGameVII(self, stones: List[int]) -> int:
+        n = len(stones)
+        flag = 1
+        lis = list(stones)
+        for i in range(1,n):
+            for j in range(n - i):
+                if flag:
+                    lis[j] = max(lis[j],lis[j+1])
+                else:
+                    lis[j] = min(lis[j] + stones[j + i],lis[j+1] + stones[j])
+            flag ^= 1
+        return lis[0] if not flag else sum(stones) - lis[0]
+        
