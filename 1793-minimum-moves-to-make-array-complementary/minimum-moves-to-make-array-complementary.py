1a894e5a257c661992211ed5e2e6277bbd575b43
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-22 19:04:24 +0530

    Time: 890 ms (72.00%) | Memory: 31.1 MB (60.00%) - LeetSync

--- 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
@@ -1,18 +1,19 @@
-from collections import defaultdict
-class Solution:
+class Solution:  
     def minMoves(self, nums: List[int], limit: int) -> int:
-        starts, ends, zero_moves = defaultdict(int), defaultdict(int), defaultdict(int)
-        N = len(nums)
-        for i in range(N // 2):
-            x, y = nums[i], nums[N-i-1]
-            starts[min(x, y)+1] += 1
-            ends[max(x,y)+limit] += 1
-            zero_moves[x+y] += 1
-        result = float('inf')
-        intervals = 0
-        for target in range(2, 2*limit+1):
-            intervals += starts[target]
-            cost = N - intervals - zero_moves[target]
-            result = min(cost, result)
-            intervals -= ends[target]
-        return result
+        n = len(nums)
+        overlay_arr = [0] * (2*limit+2)
+        for i in range(n//2):
+            left_boundary = min(nums[i], nums[n-1-i]) + 1
+            no_move_value = nums[i] + nums[n-1-i]
+            right_boundary = max(nums[i], nums[n-1-i]) + limit
+            overlay_arr[left_boundary] -= 1
+            overlay_arr[no_move_value] -= 1
+            overlay_arr[no_move_value+1] += 1
+            overlay_arr[right_boundary+1] += 1
+        curr_moves = n   #initial assumption of two moves for each pair
+        res = float("inf")
+		# start Sweeping
+        for i in range(2, 2*limit+1):
+            curr_moves += overlay_arr[i]
+            res = min(res, curr_moves)
+        return res
