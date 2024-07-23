5a5f53cb595bd08e2a9f0087d4524de47901d669
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-23 13:35:36 +0530

    Time: 372 ms (66.64%) | Memory: 40.7 MB (20.22%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -5,70 +5,20 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        curr = head
-        p1_parent = None
-        p1 = None
-        p2_parent = None
-        p2 = None
-        counter = 1
-        
-        while curr:
-            if counter == k:
-                p1 = curr
-            elif counter < k:
-                p1_parent = curr
-            
-            if counter == 1:
-                p2 = curr
-            else:
-                if counter >= k + 1:
-                    if p2_parent:
-                        p2_parent = p2_parent.next
-                    else:
-                        p2_parent = p2
-                    p2 = p2.next
-            
-            curr = curr.next
-            if curr:
-                counter += 1
 
-        if counter == 1:
-            return head
-        elif counter == 2:
-            temp2 = head.next
-            temp2.next = head
-            head.next = None
-            return temp2
-        elif k == 1:
-            p1_next = p1.next
-            p2.next = p1_next
-            p2_parent.next = p1
-            p1.next = None
-            return p2
-        elif k == counter:
-            p2_next = p2.next
-            p1.next = p2_next
-            p1_parent.next = p2
-            p2.next = None
-            return p1
-        elif p1_parent is p2:
-            p1_next = p1.next
-            p2_parent.next = p1
-            p1.next = p2
-            p2.next = p1_next
-            return head
-        elif p2_parent is p1:
-            p2_next = p2.next
-            p1_parent.next = p2
-            p2.next = p1
-            p1.next = p2_next
-            return head
-        else:
-            p1_next = p1.next
-            p2_next = p2.next
-            p1_parent.next = p2
-            p2.next = p1_next
-            p2_parent.next = p1
-            p1.next = p2_next
-            return head
-   
+        dummNode = ListNode(-1)
+        dummNode.next = head
+        firstPointer = dummNode
+        for i in range(k):
+            firstPointer = firstPointer.next
+        firstKth = firstPointer
+        secondPointer = dummNode
+
+        while(firstPointer):
+            firstPointer = firstPointer.next
+            secondPointer = secondPointer.next
+        
+        temp = secondPointer.val
+        secondPointer.val = firstKth.val
+        firstKth.val = temp
+        return dummNode.next   
