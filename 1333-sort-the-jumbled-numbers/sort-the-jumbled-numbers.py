90653b9fb38e05dfb8a0d519863621fe06c8a651
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-24 20:40:52 +0530

    Time: 804 ms (94.57%) | Memory: 24.2 MB (70.65%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -1,10 +1,17 @@
 class Solution:
     def sortJumbled(self, mapping: list[int], nums: list[int]) -> list[int]:
         def translate_integer(num: int) -> int:
-            digits: list[str] = list(str(num))
-            for i in range(len(digits)):
-                digits[i] = str(mapping[int(digits[i])])
-            return int("".join(digits))
+            if num == 0: 
+                return mapping[0]
+            res: int = 0
+            cur_mult: int = 1 
+            while num > 0:
+                digit = num % 10
+                num //= 10
+                res = mapping[digit] * cur_mult + res
+                cur_mult *= 10
+
+            return res
 
         number_mapping: dict[int, int] = {}
         for num in nums:
