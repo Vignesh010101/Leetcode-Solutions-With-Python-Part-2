9aab865e23770792d901a6e5eaaf70c7c8594c08
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-25 18:48:40 +0530

    Time: 583 ms (12.43%) | Memory: 29.4 MB (12.43%) - LeetSync

--- 1911-minimum-elements-to-add-to-form-a-given-sum/minimum-elements-to-add-to-form-a-given-sum.py
+++ 1911-minimum-elements-to-add-to-form-a-given-sum/minimum-elements-to-add-to-form-a-given-sum.py
@@ -1,3 +1,10 @@
 class Solution:
     def minElements(self, nums: List[int], limit: int, goal: int) -> int:
-        return math.ceil(abs(goal-sum(nums))/limit)
+        if sum(nums)==goal:
+            return 0
+        x=goal-sum(nums)
+        if abs(x)<limit:
+            return 1
+        count=0
+        count+=math.ceil(abs(x)/limit)
+        return count
