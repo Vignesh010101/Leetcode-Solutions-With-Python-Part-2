755c3c201dd2dd158198bc243d810a689c27655f
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-26 16:49:35 +0530

    Time: 273 ms (24.00%) | Memory: 16.5 MB (22.00%) - LeetSync

--- 1971-incremental-memory-leak/incremental-memory-leak.py
+++ 1971-incremental-memory-leak/incremental-memory-leak.py
@@ -1,10 +1,13 @@
 class Solution:
-    def memLeak(self, memory1: int, memory2: int) -> List[int]:
-        i = 1
-        while max(memory1, memory2) >= i:
-            if memory1 >= memory2:
-                memory1 -= i
+    def memLeak(self, m1: int, m2: int) -> List[int]:
+        res = [1,m1,m2]
+        while 1:
+            if res[2] > res[1]:
+                mx = 2
             else:
-                memory2 -= i
-            i += 1
-        return [i, memory1, memory2]   
+                mx = 1
+            if res[0] > res[mx]:
+                return res
+            else:
+                res[mx] -= res[0]
+            res[0]+=1   
