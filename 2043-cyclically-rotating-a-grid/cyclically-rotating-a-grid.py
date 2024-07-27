e731644ba181c66fece4f4f9d6069b69b8aaf043
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-27 16:59:48 +0530

    Time: 114 ms (70.49%) | Memory: 17.1 MB (34.43%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.py
@@ -1,21 +1,56 @@
 class Solution:
-    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        m, n = len(grid), len(grid[0]) # dimensions 
+    def rotateGrid(self, mat: List[List[int]], k: int) -> List[List[int]]:
+        top = 0
+        bottom = len(mat) - 1
+        left = 0
+        right = len(mat[0]) - 1
+        res = []
         
-        for r in range(min(m, n)//2): 
-            i = j = r
-            vals = []
-            for jj in range(j, n-j-1):     vals.append(grid[i][jj])
-            for ii in range(i, m-i-1):     vals.append(grid[ii][n-j-1])
-            for jj in range(n-j-1, j, -1): vals.append(grid[m-i-1][jj])
-            for ii in range(m-i-1, i, -1): vals.append(grid[ii][j])
-                
-            kk = k % len(vals)
-            vals = vals[kk:] + vals[:kk]
+        while left < right and top < bottom:
+            local = []
+            for i in range(left, right + 1):
+                local.append(mat[top][i])
+            top += 1
             
-            x = 0  
-            for jj in range(j, n-j-1):     grid[i][jj] = vals[x]; x += 1
-            for ii in range(i, m-i-1):     grid[ii][n-j-1] = vals[x]; x += 1
-            for jj in range(n-j-1, j, -1): grid[m-i-1][jj] = vals[x]; x += 1
-            for ii in range(m-i-1, i, -1): grid[ii][j] = vals[x]; x += 1
-        return grid
+            for i in range(top, bottom + 1):
+                local.append(mat[i][right])
+            right -= 1
+            
+            for i in range(right, left - 1, -1):
+                local.append(mat[bottom][i])
+            bottom -= 1
+            
+            for i in range(bottom, top - 1, -1):
+                local.append(mat[i][left])
+            left += 1
+            
+            res.append(local)
+        
+        for ele in res:
+            l = len(ele)
+            r = k % l
+            ele[:] = ele[r:] + ele[:r]
+        
+        top = 0
+        bottom = len(mat) - 1
+        left = 0
+        right = len(mat[0]) - 1
+        while left < right and top < bottom:
+            local = res.pop(0)
+            for i in range(left, right + 1):
+                mat[top][i] = local.pop(0)
+            top += 1
+            
+            for i in range(top, bottom + 1):
+                mat[i][right] = local.pop(0)
+            right -= 1
+            
+            for i in range(right, left - 1, -1):
+                mat[bottom][i] = local.pop(0)
+            bottom -= 1
+            
+            for i in range(bottom, top - 1, -1):
+                mat[i][left] = local.pop(0)
+            left += 1
+        
+        return mat
