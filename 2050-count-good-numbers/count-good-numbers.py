aac6d2628c782cb6a313627f17c07d9e5b1c68df
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-27 17:26:30 +0530

    Time: 34 ms (71.36%) | Memory: 16.5 MB (81.04%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,22 +1,13 @@
-import math
-
 class Solution:
-    MOD = int(1e9+7)
-
-    @staticmethod
-    def binexp(a, b, MOD):
-        a %= MOD
-        res = 1
-
-        while b > 0:
-            if b & 1:
-                res = res * a % MOD
-            a = a * a % MOD
-            b >>= 1
-
-        return res
-
     def countGoodNumbers(self, n: int) -> int:
-        odds = math.floor(n/2)
-        evens = math.ceil(n/2)
-        return int(self.binexp(5, evens, self.MOD) * self.binexp(4, odds, self.MOD) % self.MOD)
+        MOD = 10**9+7
+        if n%2==0:
+            ne=n//2
+        else:
+            ne=(n+1)//2
+        no=n//2
+    
+        te = pow(5,ne,MOD)      
+        tp = pow(4,no,MOD)      
+        return (tp*te)%MOD
+	
