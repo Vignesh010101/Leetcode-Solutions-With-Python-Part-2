a68e175365b7c5aa0f00b154199312883d420432
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 08:45:20 +0530

    Time: 934 ms (99.02%) | Memory: 31.3 MB (37.95%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        count = [0 for x in range(max(piles) + 1)]
+        for x in piles:
+            count[x] += 1
+        
+        for _ in range(k):
+            count[-1] -= 1
+            count[len(count) // 2] += 1
+            while count[-1] == 0:
+                count.pop()
+        
+        return sum(i * count[i] for i in range(len(count)))
