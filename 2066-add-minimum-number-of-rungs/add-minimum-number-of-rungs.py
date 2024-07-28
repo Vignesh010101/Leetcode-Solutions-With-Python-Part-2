ed7b8d85cacb0c0287060cd5611a6636ccb0984d
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 08:27:59 +0530

    Time: 403 ms (47.39%) | Memory: 30.2 MB (87.80%) - LeetSync

--- 2066-add-minimum-number-of-rungs/add-minimum-number-of-rungs.py
+++ 2066-add-minimum-number-of-rungs/add-minimum-number-of-rungs.py
@@ -1,6 +1,3 @@
 class Solution:
     def addRungs(self, rungs: List[int], dist: int) -> int:
-        count = (rungs[0] - 1) // dist
-        for i in range(1, len(rungs)):
-            count += (rungs[i] - rungs[i - 1] - 1) // dist
-        return count
+        return sum((a - b - 1) // dist for a, b in zip(rungs, [0] + rungs))
