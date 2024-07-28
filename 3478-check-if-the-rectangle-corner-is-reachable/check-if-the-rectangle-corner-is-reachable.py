923bbfd8cbc09efd5d1f038115a0e6a083dfee24
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 13:09:13 +0530

    Time: 2881 ms (33.33%) | Memory: 17.4 MB (66.67%) - LeetSync

--- 3478-check-if-the-rectangle-corner-is-reachable/check-if-the-rectangle-corner-is-reachable.py
+++ 3478-check-if-the-rectangle-corner-is-reachable/check-if-the-rectangle-corner-is-reachable.py
@@ -1,49 +1,20 @@
 class Solution:
-    def canReachCorner(self, X: int, Y: int, circles: List[List[int]]) -> bool:
-        vals = []
-        for xi, yi, ri in circles: 
-            if xi**2 + yi**2 <= ri**2: return False 
-            if (X-xi)**2 + (Y-yi)**2 <= ri**2: return False 
-            if xi >= X+ri or yi > Y+ri: continue 
-            vals.append([xi, yi, ri])
-        circles = vals
-        
-        n = len(circles)
-        parent = list(range(n))
-        
-        def find(p): 
-            if p != parent[p]:
-                parent[p] = find(parent[p])
-            return parent[p]
-        
-        for i in range(n): 
-            xi, yi, ri = circles[i]
-            for j in range(i+1, n): 
-                xj, yj, rj = circles[j]
-                if (xi-xj)**2 + (yi-yj)**2 <= (ri+rj)**2: 
-                    ii = find(i)
-                    jj = find(j)
-                    parent[ii] = jj 
-                    
-        group = defaultdict(list)
-        for i in range(n): 
-            ii = find(i)
-            group[ii].append(i)
-        
-        for grp in group.values(): 
-            imax = jmax = -inf
-            imin = jmin = inf 
-            inside = False
-            for i in grp:
-                i, j, r = circles[i]
-                if 0 <= i <= X or 0 <= j <= Y: inside = True 
-                imax = max(imax, i+r)
-                jmax = max(jmax, j+r)
-                imin = min(imin, i-r)
-                jmin = min(jmin, j-r)
-            if inside: 
-                if imin <= 0 and imax >= X: return False 
-                if jmin <= 0 and jmax >= Y: return False 
-                if imax >= X and jmax >= Y: return False 
-                if imin <= 0 and jmin <= 0: return False 
-        return True 
+    def canReachCorner(self, X: int, Y: int, A: List[List[int]]) -> bool:
+        def find(i):
+            if f[i] != i:
+                f[i] = find(f[i])
+            return f[i]
+
+        n = len(A)
+        f = list(range(n + 2))
+        for i in range(n):
+            x, y, r = A[i]
+            if x - r <= 0 or y + r >= Y:
+                f[find(n)] = find(i)
+            if x + r >= X or y - r <= 0:
+                f[find(n + 1)] = find(i)
+            for j in range(i):
+                x2, y2, r2 = A[j]
+                if (x - x2) ** 2 + (y - y2) ** 2 <= (r + r2) ** 2:
+                    f[find(i)] = find(j)
+        return find(n) != find(n + 1)
