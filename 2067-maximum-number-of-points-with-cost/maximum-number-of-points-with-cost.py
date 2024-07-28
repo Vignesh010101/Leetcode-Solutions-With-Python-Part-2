2a0e6e433bf18802f00395a99695b35d92948169
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 08:29:10 +0530

    Time: 2003 ms (5.47%) | Memory: 51 MB (15.85%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -1,11 +1,16 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
+        # Instead checking all possibilities, adjust upper row with max possible in each cell
         m, n = len(points), len(points[0])
-        for i in range(m - 1):
+        for i in range(1, m):
+            # Spread upper row with max possible
+            # Spread from left to right
             for j in range(1, n):
-                points[i][j] = max(points[i][j], points[i][j - 1] - 1)
-            for j in range(n - 2, -1, -1):
-                points[i][j] = max(points[i][j], points[i][j + 1] - 1)
+                points[i-1][j] = max(points[i-1][j], points[i-1][j-1]-1)
+            # Spread from right to left
+            for j in range(n-2, -1, -1):
+                points[i-1][j] = max(points[i-1][j], points[i-1][j+1]-1)
+            # Sum with upper row
             for j in range(n):
-                points[i + 1][j] += points[i][j]   
-        return max(points[m - 1])
+                points[i][j] += points[i-1][j]
+        return max(points[m-1])
