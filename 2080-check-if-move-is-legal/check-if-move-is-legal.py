c9f9bc95d58fa42560cf2b60118595c51b4692a1
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 08:42:33 +0530

    Time: 46 ms (92.54%) | Memory: 16.7 MB (19.66%) - LeetSync

--- 2080-check-if-move-is-legal/check-if-move-is-legal.py
+++ 2080-check-if-move-is-legal/check-if-move-is-legal.py
@@ -1,11 +1,24 @@
 class Solution:
     def checkMove(self, board: List[List[str]], rMove: int, cMove: int, color: str) -> bool:
-        for di, dj in (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1): 
-            i, j = rMove+di, cMove+dj
-            step = 0
-            while 0 <= i < 8 and 0 <= j < 8: 
-                if board[i][j] == color and step: return True 
-                if board[i][j] == "." or board[i][j] == color and not step: break 
-                i, j = i+di, j+dj
-                step += 1
+
+        board = [['.']*10] + [                     
+                 ['.']+row+['.'] for row in board]+ [ 
+                 ['.']*10]
+
+        other = 'B' if color == 'W' else 'W'
+
+        dir = ((-1,-1), (-1,0), (-1, 1),            
+               ( 0,-1),         ( 0, 1),
+               ( 1,-1), ( 1,0), ( 1, 1))
+
+        for dr, dc in dir:
+
+            r, c = rMove + 1 + dr, cMove + 1 + dc   
+            if board[r][c] != other: continue       
+
+            while board[r][c] == other:             
+                r+= dr                             
+                c+= dc
+
+            if board[r][c] == color: return True    
         return False
