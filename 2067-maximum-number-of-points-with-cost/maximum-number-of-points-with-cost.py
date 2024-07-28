24fc54779f41529970f74060d059f8b1357f6438
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-28 08:31:08 +0530

    Time: 2515 ms (5.47%) | Memory: 51.2 MB (10.93%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.py
@@ -1,23 +1,8 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        def goright(row): 
-            for i in range(1, len(row)):
-                if row[i-1] - 1 > row[i]: 
-                    row[i] = row[i-1]-1
-
-        def goleft(row): 
-            for i in range(len(row)-2, -1, -1):
-                if row[i+1]-1 > row[i]:
-                    row[i] = row[i+1]-1
-
-        goright(points[0])
-        goleft(points[0])
-
-        res = 0
-        for i in range(1, len(points)):
-            for j in range(len(points[i])):
-                points[i][j] += points[i-1][j]
-            goright(points[i])
-            goleft(points[i])
-
-        return max(points[-1])
+        n = len(points[0])
+        def mySum(row1, row2):
+            for i in range(1,n): row1[i] = max(row1[i], row1[i-1] - 1)
+            for i in range(n-2, -1, -1): row1[i] = max(row1[i], row1[i+1] - 1)
+            return [x+y for x, y in zip(row1, row2)]
+        return max(list(reduce(lambda row1, row2: mySum(row1, row2), points[::-1])))
