3c197996867e1c69552f7cbd30bb87733ccaf29d
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-29 20:20:35 +0530

    Time: 798 ms (55.00%) | Memory: 31.4 MB (35.00%) - LeetSync

--- 2124-first-day-where-you-have-been-in-all-the-rooms/first-day-where-you-have-been-in-all-the-rooms.py
+++ 2124-first-day-where-you-have-been-in-all-the-rooms/first-day-where-you-have-been-in-all-the-rooms.py
@@ -1,8 +1,10 @@
 class Solution:
     def firstDayBeenInAllRooms(self, nextVisit: List[int]) -> int:
-        odd = [0]
-        even = [1]
-        for i in range(1, len(nextVisit)): 
-            odd.append((even[-1] + 1) % 1_000_000_007)
-            even.append((2*odd[-1] - odd[nextVisit[i]] + 1) % 1_000_000_007)
-        return odd[-1] 
+        MOD = 10**9+7
+        n =len(nextVisit)
+        dp=[0]*n
+        for i in range(n-1):
+            dp[i+1] += (2*dp[i] - dp[nextVisit[i]] + 2)%MOD
+    
+        return dp[-1]
+	
