c7914b22d10af6329c9dfab42c6f4c7fe9901f69
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-29 20:30:16 +0530

    Time: 130 ms (99.01%) | Memory: 18.6 MB (18.03%) - LeetSync

---
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProduct(self, s: str) -> int:
+        @cache
+        def dp(s):
+            if len(s) == 0:
+                return 0
+            if len(s) == 1:
+                return 1
+            else:
+                if s[0]==s[-1]:
+                    return dp(s[1:-1])+2
+                else:
+                    return max(dp(s[1:]),dp(s[:-1]))
+        ans = 0
+        def generate(s1,s2,ind):
+            if ind == len(s):
+                nonlocal ans
+                ans=max(ans,dp(s1)*dp(s2))
+                return
+            generate(s1+s[ind],s2,ind+1)
+            generate(s1,s2+s[ind],ind+1)
+        
+        generate('','',0)
+        return ans
+        
