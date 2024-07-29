15dde888e7825acfd32d5f889524daeb4b16da7d
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-07-29 19:36:13 +0530

    Time: 713 ms (76.81%) | Memory: 25.7 MB (10.87%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -1,10 +1,7 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        ans = mult = 0
-        val = inf 
-        for i in range(len(matrix)): 
-            for j in range(len(matrix)):
-                ans += abs(matrix[i][j])
-                val = min(val, abs(matrix[i][j]))
-                if matrix[i][j] < 0: mult ^= 1
-        return ans - 2*mult*val
+        arr = tuple(chain(*matrix))
+        hasOddNeg = reduce(operator.xor, map(lambda x: x < 0, arr))
+        absArr = tuple(map(abs, arr))
+        res = sum(absArr) - 2 * hasOddNeg * min(absArr)
+        return res
