1458576b1feb43000e66d42860dde2248e992e22
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-01 14:48:09 +0530

    Time: 2524 ms (29.31%) | Memory: 56.6 MB (62.07%) - LeetSync

--- 2250-k-highest-ranked-items-within-a-price-range/k-highest-ranked-items-within-a-price-range.py
+++ 2250-k-highest-ranked-items-within-a-price-range/k-highest-ranked-items-within-a-price-range.py
@@ -1,25 +1,28 @@
+import heapq
 class Solution:
     def highestRankedKItems(self, grid: List[List[int]], pricing: List[int], start: List[int], k: int) -> List[List[int]]:
-        m, n = len(grid), len(grid[0])
-        sx, sy = start
-        pl, ph = pricing
-        q = [(0, grid[sx][sy], sx, sy)]
-        visited = set()
-        res = []
-
+        row, col = len(grid), len(grid[0])
+        q = [(0, grid[start[0]][start[1]], start[0], start[1])]
+        visit = set()
+        result = []
         while q:
             dis, price, x, y = heapq.heappop(q)
-            if (x, y) in visited: continue
-            
-            visited.add((x, y))
-            
-            if price != 1 and pl <= price <= ph: res.append([x, y])
-            if len(res) == k: return res
-
-            for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            if (x, y) in visit:
+                continue
+            visit.add((x, y))
+            if price != 1 and price >= pricing[0] and price <= pricing[1]:
+                result.append([x, y])
+            if len(result) == k:
+                return result
+            for dx, dy in (0, 1), (0, -1), (1, 0), (-1, 0):
                 new_x, new_y = x + dx, y + dy
-                if 0 <= new_x < m and 0 <= new_y < n:
-                    val = grid[new_x][new_y]
-                    if val == 0: continue
-                    else: heapq.heappush(q, (dis + 1, val, new_x, new_y))
-        return res
+                if 0 <= new_x < row and 0 <= new_y < col and (new_x, new_y) not in visit:
+                    temp = grid[new_x][new_y]
+                    if temp == 0:
+                        continue
+                    elif temp == 1:
+                        heapq.heappush(q, (dis + 1, 1, new_x, new_y))
+                    else:
+                        heapq.heappush(q, (dis + 1, temp, new_x, new_y))
+        
+        return result
