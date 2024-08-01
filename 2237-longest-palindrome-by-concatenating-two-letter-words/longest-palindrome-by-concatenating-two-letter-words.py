58d9fb11db7adb7ed2e5c90ea567acd9c04ce02c
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-01 14:30:47 +0530

    Time: 1011 ms (95.65%) | Memory: 40.8 MB (38.38%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,3 +1,12 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        return (lambda counts : 2*(sum(min(counts[word], counts[word[::-1]]) if word != word[::-1] else counts[word] - (counts[word] % 2) for word in counts.keys())+int(any(word == word[::-1] and counts[word] % 2 for word in counts.keys()))))(Counter(words))
+        counts  = Counter(words)
+        has_double = False
+        for word in counts.keys():
+            if word != word[::-1]:
+                counts[word] = min(counts[word], counts[word[::-1]])
+            else:
+                if counts[word] % 2:
+                    has_double = True
+                counts[word] = counts[word] - (counts[word] % 2)
+        return 2*(sum(counts.values())+int(has_double))
