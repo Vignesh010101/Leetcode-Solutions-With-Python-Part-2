71ef2976791fe084d7ea875f34eb1996b4809144
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-02 20:55:38 +0530

    Time: 118 ms (92.04%) | Memory: 17.5 MB (34.51%) - LeetSync

--- 2309-maximize-number-of-subsequences-in-a-string/maximize-number-of-subsequences-in-a-string.py
+++ 2309-maximize-number-of-subsequences-in-a-string/maximize-number-of-subsequences-in-a-string.py
@@ -1,12 +1,24 @@
 class Solution:
     def maximumSubsequenceCount(self, text: str, pattern: str) -> int:
-        count = 0
-        ans1 = 0
-        ans2 = 0
-        for i in range(len(text)-1, -1, -1):
-            if text[i] == pattern[0]:
-                ans1 += count + 1
-                ans2 += count
-            if text[i] == pattern[1]:
-                count += 1
-        return max(ans1, ans2 + count)
+        
+        a = pattern[0]
+        b = pattern[1]
+        if a==b:
+            count = 1
+            for e in text:
+                if e == a:
+                    count += 1
+                
+            return int(count*(count-1)/2)
+        count_a = 0
+        count_b = 0
+        total_count = [0,0]
+        for e in text:
+            if e == a:
+                count_a += 1
+            elif e == b:
+                count_b += 1
+                total_count[0] += count_a
+                total_count[1] += count_a+1
+        
+        return max(total_count[0]+count_a, total_count[1])
