9d9aa823603c7d843e2cd513edf80e7c02de4a33
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-02 20:54:23 +0530

    Time: 1740 ms (48.94%) | Memory: 66.1 MB (52.13%) - LeetSync

--- 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
+++ 1312-count-artifacts-that-can-be-extracted/count-artifacts-that-can-be-extracted.py
@@ -1,16 +1,20 @@
 class Solution:
-    def digArtifacts(self, n, artifacts, dig):
-
-        dig = set((r,c) for r,c in dig)
-
-        count =0
-        for r1,c1, r2,c2 in artifacts:
-            positions = set()
-            for r in range(r1, r2+1):
-                for c in range(c1, c2+1):
-                    positions.add((r,c))
-
-            if all([pos in dig for pos in positions]):
-                count+=1
-
+    def digArtifacts(self, n: int, artifact: List[List[int]], dig: List[List[int]]) -> int:
+        dig_set = set()
+        for i in dig:
+            dig_set.add(tuple(i))
+            
+        count = 0
+        for afact in artifact:
+            flag = True
+            r1,c1,r2,c2 = afact
+            for i in range(r1,r2+1):
+                for j in range(c1,c2+1):
+                    if (i,j) not in dig_set:
+                        flag = False
+                        break
+                        
+            if(flag == True):
+                count += 1
+                
         return count
