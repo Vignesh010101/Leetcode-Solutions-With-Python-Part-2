e28f86ce0b4db1958e61519045e2d1ab0cd578d8
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-03 18:47:38 +0530

    Time: 105 ms (95.80%) | Memory: 21.4 MB (66.03%) - LeetSync

--- 2348-count-number-of-texts/count-number-of-texts.py
+++ 2348-count-number-of-texts/count-number-of-texts.py
@@ -1,19 +1,39 @@
 class Solution:
     def countTexts(self, pressedKeys: str) -> int:
-        MOD = 1_000_000_007 
-        
-        @cache 
-        def fn(n, k): 
-            """Return number of possible text of n repeated k times."""
-            if n < 0: return 0
-            if n == 0: return 1
-            ans = 0
-            for x in range(1, k+1): ans = (ans + fn(n-x, k)) % MOD
-            return ans 
-        
         ans = 1
-        for key, grp in groupby(pressedKeys): 
-            if key in "79": k = 4
-            else: k = 3
-            ans = (ans * fn(len(list(grp)), k)) % MOD 
-        return ans 
+        modulo = 10 ** 9 + 7
+        
+        # Sequences of digits with 3 & 4 letters
+        seqs = {3: [], 4: []}
+        
+        # DP for basic cases
+        dp = {
+            3: [1, 2, 4],  # Keys with 3 letters (2, 3, 4, 5, 6, 8)
+            4: [1, 2, 4, 8]  # Keys with 4 letters (7, 9)
+        }
+        
+        # Append and store sequences of 7 and 9 separately
+        def appendSeq(key):
+            seqs[4 if key in {'7', '9'} else 3].append(curSeq)
+
+        # Iterate the pressed keys and store the length of each subsequence
+        curSeq, curKey = 1, pressedKeys[0]
+        for key in pressedKeys[1:]:
+            if not curKey == key:
+                appendSeq(curKey)
+                curKey, curSeq = key, 0
+            curSeq += 1
+        appendSeq(curKey)
+        
+        for k in seqs.keys():
+            # Update the DP with new sequences (only consider the maximum seqeunce length)
+            if seqs[k]:
+                for i in range(k, max(seqs[k])):
+                    dp[k].append(sum(dp[k][(i - k):i]) % modulo)
+
+            # Update the final result using DP & given modulo
+            for seq in seqs[k]:
+                ans *= dp[k][seq - 1]
+                ans %= modulo
+
+        return ans
