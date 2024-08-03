cace9812a64c594472673cb3fd29b9a7915c4aae
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-03 16:15:12 +0530

    Time: 39 ms (41.14%) | Memory: 16.5 MB (72.29%) - LeetSync

--- 2328-minimize-result-by-adding-parentheses-to-expression/minimize-result-by-adding-parentheses-to-expression.py
+++ 2328-minimize-result-by-adding-parentheses-to-expression/minimize-result-by-adding-parentheses-to-expression.py
@@ -1,16 +1,9 @@
 class Solution:
     def minimizeResult(self, expression: str) -> str:
-        plus_pos = expression.find('+')
-        min_val, min_lp, min_rp = None, 0, 0
-
-        for lp in range(0, plus_pos):
-            for rp in range(plus_pos+2, len(expression)+1):
-                val = int(expression[:lp] or 1) * \
-                    (int(expression[lp:plus_pos]) + int(expression[plus_pos+1:rp])) * \
-                    int(expression[rp:] or 1)
-                if min_val is None or val < min_val:
-                    min_val = val
-                    min_lp = lp
-                    min_rp = rp
+        left, right = expression.split('+')
+        value = lambda s: eval(s.replace('(','*(').replace(')',')*').strip('*'))
         
-        return expression[:min_lp] + '(' + expression[min_lp:min_rp] + ')' + expression[min_rp:]
+        lft = [left[0:i]+'('+left[i:] for i in range(len(left))]
+        rgt = [right[0:i]+')'+right[i:] for i in range(1, len(right)+1)]
+       
+        return min([l+'+'+r for l in lft for r in rgt], key=value)
