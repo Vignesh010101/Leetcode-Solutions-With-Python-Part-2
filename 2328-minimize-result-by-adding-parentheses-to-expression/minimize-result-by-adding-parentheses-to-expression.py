a9801c2009e071868f698e52c0532b87ff9137e1
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-03 16:16:15 +0530

    Time: 42 ms (24.29%) | Memory: 16.6 MB (37.71%) - LeetSync

--- 2328-minimize-result-by-adding-parentheses-to-expression/minimize-result-by-adding-parentheses-to-expression.py
+++ 2328-minimize-result-by-adding-parentheses-to-expression/minimize-result-by-adding-parentheses-to-expression.py
@@ -1,25 +1,9 @@
 class Solution:
     def minimizeResult(self, expression: str) -> str:
-
-        def eval_expr(l,li,ri):
-            ai=1
-            if li!=0:
-                l.insert(li,'*')
-                ai+=1
-            if ri!=len(l)-ai:
-                l.insert(ri+ai,'*')
-            return eval(''.join(l))
-
-        pi=expression.index('+')
-        length=len(expression)
-        res_sum,res_s=float('inf'),None
-        for i in range(pi):
-            for j in range(pi+2,length+1):
-                l=list(expression)
-                l.insert(i,'(')
-                l.insert(j+1,')')
-                t=eval_expr(l,i,j+1)
-                if t<res_sum:
-                    res_sum=t
-                    res_s=''.join([c for c in l if c!='*'])
-        return res_s
+        k = expression.find('+')
+        ans = "inf"
+        for i in range(k): 
+            for j in range(k+1, len(expression)): 
+                cand = f'{expression[:i]}({expression[i:k]}+{expression[k+1:j+1]}){expression[j+1:]}'
+                ans = min(ans, cand, key=lambda x: eval(x.replace('(', '*(').replace(')', ')*').strip('*')))
+        return ans 
