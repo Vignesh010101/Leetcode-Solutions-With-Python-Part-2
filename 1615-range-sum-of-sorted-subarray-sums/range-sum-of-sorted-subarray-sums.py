3b7f464f3f41817311855b6700741967d7d62841
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-04 08:22:29 +0530

    Time: 205 ms (87.61%) | Memory: 39.9 MB (51.33%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.py
@@ -1,19 +1,10 @@
-class Solution(object):
-    def rangeSum(self, nums, n, left, right):
-        prefix = [0] * (n + 1)
-        for i in range(n):
-            prefix[i + 1] = prefix[i] + nums[i]
-        
-        minHeap = []
-
-        for i in range(1, n + 1):
-            for j in range(i):
-                heapq.heappush(minHeap, prefix[i] - prefix[j])
-        
-        result = 0
-        for i in range(1, right + 1):
-            sum_value = heapq.heappop(minHeap)
-            if i >= left:
-                result = (result + sum_value) % 1000000007
-        
-        return result
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        ans = []
+        for i in range(len(nums)):
+            prefix = 0
+            for ii in range(i, len(nums)):
+                prefix += nums[ii]
+                ans.append(prefix)
+        ans.sort()
+        return sum(ans[left-1:right]) % 1_000_000_007
