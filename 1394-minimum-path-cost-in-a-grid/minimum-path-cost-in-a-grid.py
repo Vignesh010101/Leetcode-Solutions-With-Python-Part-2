756f9adf2b68881902b48e9ba20c3a11b653de32
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-05 12:37:25 +0530

    Time: 1632 ms (80.97%) | Memory: 22.4 MB (48.06%) - LeetSync

--- 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.py
+++ 1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.py
@@ -1,13 +1,7 @@
 class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        
-        @cache
-        def fn(i, j):
-            if i == m-1: return grid[i][j]
-            ans = inf 
-            for jj in range(n): 
-                ans = min(ans, grid[i][j] + fn(i+1, jj) + moveCost[grid[i][j]][jj])
-            return ans 
-        
-        return min(fn(0, j) for j in range(n))
+        n = len(grid[0])
+        for i in range(len(grid)-2, -1 ,-1):
+            for j in range(n):
+                grid[i][j] += min([moveCost[grid[i][j]][cols] + grid[i+1][cols] for cols in range(n)])
+        return min(grid[0])
