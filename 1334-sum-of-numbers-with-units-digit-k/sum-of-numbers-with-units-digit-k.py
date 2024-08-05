c8accacd612ad8620f4c5572b83ee18194874ac3
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-05 12:43:24 +0530

    Time: 32 ms (77.12%) | Memory: 16.5 MB (54.25%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.py
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.py
@@ -1,23 +1,25 @@
 class Solution:
-    def minimumNumbers(self, num: int, k: int) -> int:
+    def minimumNumbers(self, num: int, k: int) -> int:        
         if num == 0:
             return 0
-        if k == 0:
-            if num % 10 == 0:
-                return 1
+        
+        target_last_digit = num % 10
+        cur_sum = k
+        min_size = 1
+        visited = set()
+        
+        while cur_sum <= num:
+            cur_last_digit = cur_sum % 10
+            if cur_last_digit == target_last_digit:
+                return min_size
+            
             else:
-                return -1
-        cycles = {1:[0,1,2,3,4,5,6,7,8,9], 2:[0, 2,4,6,8], 3:[0, 3,6,9,2,5,8,1,4,7], 4:[0, 4,8,2,6], 5:[0,5],6:[0,6,2,8,4],7:[0,7,4,1,8,5,2,9,6,3],8:[0,8,6,4,2],9:[0,9,8,7,6,5,4,3,2,1]}
-        l_digit = num % 10
-        if l_digit not in cycles[k]:
-            return -1
-        ind = cycles[k].index(l_digit)
-        if ind == 0:
-            ind = len(cycles[k])
-        if num < k * ind:
-            return -1
-        return ind
+                if cur_last_digit in visited:
+                    return -1
+                
+                visited.add(cur_last_digit)
+            
+            cur_sum += k
+            min_size += 1
         
-        
-
-        
+        return -1
