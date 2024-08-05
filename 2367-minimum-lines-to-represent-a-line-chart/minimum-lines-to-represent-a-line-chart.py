8123869223e1cd8af8babcc195c6f38dfd3910db
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-05 12:31:33 +0530

    Time: 1134 ms (91.28%) | Memory: 61.4 MB (63.59%) - LeetSync

--- 2367-minimum-lines-to-represent-a-line-chart/minimum-lines-to-represent-a-line-chart.py
+++ 2367-minimum-lines-to-represent-a-line-chart/minimum-lines-to-represent-a-line-chart.py
@@ -1,13 +1,29 @@
-from fractions import Fraction
+import math
 class Solution:
     def minimumLines(self, stockPrices: List[List[int]]) -> int:
-        l=[]
-        stockPrices.sort()
-        c=None
-        k=0
-        for i,j in pairwise(stockPrices):
-            x=Fraction(j[1]-i[1],j[0]-i[0])
-            if x!=c:
-                k+=1
-                c=x
-        return k
+        stockPrices = sorted(stockPrices, key=lambda xy: xy[0])
+
+        num_lines = 0
+        curr_line = None
+        for i in range(1, len(stockPrices)):
+            x1, y1 = stockPrices[i]
+            if curr_line is None:
+                x0, y0 = stockPrices[i-1]
+                dx = x1 - x0
+                dy = y1 - y0
+
+                curr_line = (dx, dy, x0, y0)
+                num_lines += 1
+            else:
+                dxl, dyl, xl, yl = curr_line
+                dxl0 = x1 - xl
+                dyl0 = y1 - yl
+                if dxl0 * dyl != dyl0 * dxl:
+                    x0, y0 = stockPrices[i-1]
+                    dx = x1 - x0
+                    dy = y1 - y0
+
+                    curr_line = (dx, dy, x0, y0)
+                    num_lines += 1
+
+        return num_lines
