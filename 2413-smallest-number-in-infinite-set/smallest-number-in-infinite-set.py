5e98f62ae726729b9038f4603d4dd828d961580a
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-06 20:53:14 +0530

    Time: 93 ms (65.96%) | Memory: 17.4 MB (18.48%) - LeetSync

--- 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.py
@@ -1,20 +1,31 @@
-from sortedcontainers import SortedList 
-
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.x = 1
-        self.seen = SortedList()
+        self.min_val = 1
+        self.is_val_in_heap = {}
+        #self.is_val_in_heap = set()
+        self.heap = []
 
     def popSmallest(self) -> int:
-        if self.seen: return self.seen.pop(0)
-        else: 
-            self.x += 1
-            return self.x - 1
+        if not self.heap:
+            smallest = self.min_val
+            self.min_val +=1
+        else:
+            smallest = heappop(self.heap)
+            del self.is_val_in_heap[smallest]
+            #self.is_val_in_heap.remove(smallest)
+        
+        return smallest
 
     def addBack(self, num: int) -> None:
-        if num < self.x and num not in self.seen: 
-            self.seen.add(num)
+        if num >= self.min_val or num in self.is_val_in_heap:
+            return
+        elif num == self.min_val - 1:
+            self.min_val -= 1
+        else:
+            heappush(self.heap, num)
+            self.is_val_in_heap[num] = True
+            #self.is_val_in_heap.add(num)
 
 # Your SmallestInfiniteSet object will be instantiated and called as such:
 # obj = SmallestInfiniteSet()
