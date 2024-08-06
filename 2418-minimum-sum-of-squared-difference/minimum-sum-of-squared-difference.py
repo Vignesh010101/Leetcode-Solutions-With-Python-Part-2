8f53cb7d39bd0012f80fb10806c33978fdef4f24
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-06 20:51:34 +0530

    Time: 748 ms (98.06%) | Memory: 34.5 MB (42.58%) - LeetSync

--- 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.py
+++ 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.py
@@ -1,29 +1,30 @@
+from collections import Counter
+
 class Solution:
     def minSumSquareDiff(self, nums1: List[int], nums2: List[int], k1: int, k2: int) -> int:
-        dif = [abs(nums2[i] - nums1[i]) for i in range(len(nums2))]
-        dif.sort(reverse=True)
-        dif.append(0)
-        pre = 0
-        index = -1
-        for i in range(1, len(dif)):
-            after = pre + (dif[i - 1] - dif[i]) * i
-            if after > k1 + k2:
-                index = i
-                break
-            pre = after
-        if index == -1:
-            return 0
-        excess = k1 + k2 - pre
-        if index > 0:
-            mod = excess % index
-        else:
-            mod = 0
-        for i in range(mod):
-            dif[i] = dif[index - 1] - (excess + index - 1) // index
-        for i in range(mod, index):
-            dif[i] = dif[index - 1] - excess // index
+        diff = [abs(num1 - num2) for num1, num2 in zip(nums1, nums2)]
 
-        out = 0
-        for i in range(len(dif)):
-            out += dif[i]**2
-        return out
+        k = k1+ k2
+        if sum(diff) <= k: return 0
+
+        diffCount = Counter(diff)
+        diff = sorted(diffCount.keys(), reverse = True)
+        n = len(diff)
+
+        i = 0; count = diffCount[diff[i]]
+
+        while i+1 < n and (diff[i] - diff[i+1])*count <= k:
+            k -= (diff[i] - diff[i+1])*count
+            count += diffCount[diff[i+1]]
+            i += 1
+
+        def findResidualSum(k, num, count):
+            if k >= num*count: return 0
+
+            k, num = k%count, num - k//count
+            ret = k*(num-1)**2 + (count-k)*num**2
+            return ret
+
+        res = findResidualSum(k, diff[i], count)
+        ret = res + sum([diffCount[d]*d**2 for d in diff[i+1:]])
+        return ret
