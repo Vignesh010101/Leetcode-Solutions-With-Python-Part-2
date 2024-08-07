2a192e0591982683a5fa6001a24a500e59849c07
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-07 21:38:47 +0530

    Time: 45 ms (85.33%) | Memory: 16.4 MB (97.33%) - LeetSync

--- 2482-maximum-rows-covered-by-columns/maximum-rows-covered-by-columns.py
+++ 2482-maximum-rows-covered-by-columns/maximum-rows-covered-by-columns.py
@@ -1,34 +1,3 @@
 class Solution:
-    def maximumRows(self, mat: List[List[int]], cols: int) -> int:
-        res = []
-        M = len(mat)
-        N = len(mat[0])
-        def check(seen):
-            count = 0
-            for row in mat:
-                flag = True
-                for c in range(N): 
-                    if row[c] == 1:
-                        if c in seen:
-                            continue
-                        else:
-                            flag = False
-                            break
-                if flag:    
-                    count +=1   
-            res.append(count)
-                     
-        def solve(c,seen,cols):
-            if cols == 0:
-                check(seen)
-                return
-            if c == N:
-                return
-            else:
-                seen.add(c)
-                solve(c+1,seen,cols-1)
-                seen.remove(c)
-                solve(c+1,seen,cols)
-        seen = set()
-        solve(0,seen,cols)
-        return max(res)
+    def maximumRows(self, m: List[List[int]], k: int) -> int:
+        return (n:=len(m[0])) and max(sum(all(r[c]==0 for c in q) for r in m) for q in combinations([*range(n)],n-k))
