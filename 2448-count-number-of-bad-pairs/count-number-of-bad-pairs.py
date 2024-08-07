f6a847b3b976c4947449f9786ac876e0cdd36ce2
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-07 20:53:25 +0530

    Time: 501 ms (86.69%) | Memory: 36.3 MB (15.01%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,14 +1,15 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        nums_len = len(nums)
-        count_dict = dict()
-        for i in range(nums_len):
-            nums[i] -= i
-            if nums[i] not in count_dict:
-                count_dict[nums[i]] = 0
-            count_dict[nums[i]] += 1
+        data= {}
+        total = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] - i not in data:
+                data[nums[i]-i] =1
+            else:
+                data[nums[i]-i] +=1
+        for key in data:
+            if data[key]>1:
+                total +=(data[key]*(data[key]-1))//2
+        return ((n*(n-1)//2)-total)
         
-        count = 0
-        for key in count_dict:
-            count += math.comb(count_dict[key], 2)
-        return math.comb(nums_len, 2) - count
