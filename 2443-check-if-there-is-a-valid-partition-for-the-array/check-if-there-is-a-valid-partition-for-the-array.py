790b9fedc27a98e975553352d196efb6d4443aaf
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-07 20:56:46 +0530

    Time: 724 ms (67.96%) | Memory: 80 MB (14.63%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,18 +1,21 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        n = len(nums) 
-        if n == 1: 
-            return False 
-        dp = [True, False, nums[0] == nums[1] if n > 1 else False] 
+        n=len(nums)
+        found=False
+        from functools import cache
 
-        for i in range(2, n): 
-            current_dp = False 
-            if nums[i] == nums[i-1] and dp[1]: 
-                current_dp = True 
-            elif nums[i] == nums[i-1] == nums[i-2] and dp[0]: 
-                current_dp = True 
-            elif nums[i] - nums[i-1] == 1 and nums[i-1] - nums[i-2] == 1 and dp[0]: 
-                current_dp = True  
-            dp[0], dp[1], dp[2] = dp[1], dp[2], current_dp 
-
-        return dp[2]
+        @cache
+        def solve(i):
+            nonlocal n,nums,found
+            if i<0:
+                return True
+            if not found:
+                res=False
+                if i-1>=0 and nums[i]==nums[i-1]:
+                    res=res or solve(i-2)
+                if i-2>=0 and (nums[i-2]==nums[i-1]==nums[i] or nums[i-2]+2==nums[i-1]+1==nums[i]):
+                    res=res or solve(i-3)
+                found=found or res
+                return res
+        
+        return solve(n-1)
