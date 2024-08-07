78a4f5e448a5c77022a1c4a10104ef7219899a36
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-07 21:37:33 +0530

    Time: 54 ms (48.00%) | Memory: 16.7 MB (30.00%) - LeetSync

--- 2482-maximum-rows-covered-by-columns/maximum-rows-covered-by-columns.py
+++ 2482-maximum-rows-covered-by-columns/maximum-rows-covered-by-columns.py
@@ -1,12 +1,34 @@
 class Solution:
     def maximumRows(self, mat: List[List[int]], cols: int) -> int:
-        m, n = len(mat), len(mat[0])
-        masks = []
-        for i in range(m): 
-            mask = reduce(xor, (1<<j for j in range(n) if mat[i][j]), 0)
-            masks.append(mask)
-        ans = 0 
-        for x in range(1<<n): 
-            if x.bit_count() <= cols: 
-                ans = max(ans, sum(mask & x == mask for mask in masks))
-        return ans 
+        res = []
+        M = len(mat)
+        N = len(mat[0])
+        def check(seen):
+            count = 0
+            for row in mat:
+                flag = True
+                for c in range(N): 
+                    if row[c] == 1:
+                        if c in seen:
+                            continue
+                        else:
+                            flag = False
+                            break
+                if flag:    
+                    count +=1   
+            res.append(count)
+                     
+        def solve(c,seen,cols):
+            if cols == 0:
+                check(seen)
+                return
+            if c == N:
+                return
+            else:
+                seen.add(c)
+                solve(c+1,seen,cols-1)
+                seen.remove(c)
+                solve(c+1,seen,cols)
+        seen = set()
+        solve(0,seen,cols)
+        return max(res)
