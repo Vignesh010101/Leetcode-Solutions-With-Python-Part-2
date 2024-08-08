d096564d07630e0b263bc8485907329a2ffd59e9
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-08 13:06:11 +0530

    Time: 392 ms (78.18%) | Memory: 71.8 MB (63.64%) - LeetSync

---
+++ 2512-longest-uploaded-prefix/longest-uploaded-prefix.py
@@ -0,0 +1,19 @@
+class LUPrefix:
+
+    def __init__(self, n: int):
+        self.videos = [False] * (n + 1)
+        self.prefix = 0
+
+    def upload(self, video: int) -> None:
+        self.videos[video-1] = True
+
+    def longest(self) -> int:
+        while self.videos[self.prefix]:
+            self.prefix += 1
+        return self.prefix        
+
+
+# Your LUPrefix object will be instantiated and called as such:
+# obj = LUPrefix(n)
+# obj.upload(video)
+# param_2 = obj.longest()
