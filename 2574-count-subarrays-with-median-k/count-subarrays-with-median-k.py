9e8c90780d9e358fc01eb76ce70b561dcf9571a1
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-10 12:01:11 +0530

    Time: 364 ms (62.99%) | Memory: 30.3 MB (83.46%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
@@ -1,13 +1,25 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        idx = nums.index(k)
-        freq = Counter()
-        prefix = 0
-        for i in reversed(range(idx+1)): 
-            prefix += int(nums[i] > k) - int(nums[i] < k)
-            freq[prefix] += 1
-        ans = prefix = 0 
-        for i in range(idx, len(nums)): 
-            prefix += int(nums[i] > k) - int(nums[i] < k)
-            ans += freq[-prefix] + freq[-prefix+1]
-        return ans 
+        index_k,n = -1, len(nums)
+        for i in range(len(nums)):
+            if nums[i]==k:
+                index_k = i
+                break
+        d = defaultdict(int)
+        current_sum = 0
+        for x in range(index_k, n):
+            if nums[x]>k:
+                current_sum+=1
+            elif nums[x]<k:
+                current_sum-=1
+                
+            d[current_sum]+=1
+        result, current_sum = 0,0
+        for x in range(index_k, -1,-1):
+            if nums[x]>k:
+                current_sum+=1
+            elif nums[x]<k:
+                current_sum-=1
+            result+= d[-1*current_sum] + d[1-current_sum]
+        return result
+            
