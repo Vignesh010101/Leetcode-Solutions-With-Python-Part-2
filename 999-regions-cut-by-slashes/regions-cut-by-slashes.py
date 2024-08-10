fb05f883ef0f4522ce6a5f7ac3c576c4959883bd
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-10 10:14:57 +0530

    Time: 46 ms (97.63%) | Memory: 16.6 MB (92.49%) - LeetSync

--- 999-regions-cut-by-slashes/regions-cut-by-slashes.py
+++ 999-regions-cut-by-slashes/regions-cut-by-slashes.py
@@ -1,49 +1,60 @@
-class UnionFind:
-    def __init__(self, r, c):
-        self.ar = [[[-1, -1] for _ in range(c)] for _ in range(r)]
-    
-    def find(self, i, j, k):
-        while self.ar[i][j][k] != -1:
-            (i, j, k) = self.ar[i][j][k]
-        return (i, j, k)
-
-    def union(self, i, j, k, l, m, n):
-        p1, p2 = self.find(i, j, k), self.find(l, m, n)
-        if p1 == p2:return
-        self.ar[p2[0]][p2[1]][p2[2]] = p1 
-
-
 class Solution:
     def regionsBySlashes(self, grid: List[str]) -> int:
-        r, c = len(grid), len(grid[0])
-        uf = UnionFind(r, c)
-        for i in range(r):
-            for j in range(c):
-                if j + 1 < c:
-                    uf.union(i, j, 1, i, j + 1, 0)
+        grid_size = len(grid)
+        points_per_side = grid_size + 1
+        total_points = points_per_side * points_per_side
 
-                if grid[i][j] == ' ':
-                    uf.union(i, j, 0, i, j, 1)
-                    if i + 1 < r:
-                        if grid[i + 1][j] == ' ' or grid[i + 1][j] == '/':
-                            uf.union(i, j, 0, i + 1, j, 0)
-                        else:
-                            uf.union(i, j, 0, i + 1, j, 1)
-                elif grid[i][j] == '/':
-                    if i + 1 < r:
-                        if grid[i + 1][j] == ' ' or grid[i + 1][j] == '/':
-                            uf.union(i, j, 1, i + 1, j, 0)
-                        else:
-                            uf.union(i, j, 1, i + 1, j, 1)
-                else:
-                    if i + 1 < r:
-                        if grid[i + 1][j] == ' ' or grid[i + 1][j] == '/':
-                            uf.union(i, j, 0, i + 1, j, 0)
-                        else:
-                            uf.union(i, j, 0, i + 1, j, 1)
-        ans = 0
-        for i in range(r):
-            for j in range(c):
-                if uf.ar[i][j][0] == -1:ans += 1
-                if uf.ar[i][j][1] == -1:ans += 1
-        return ans
+        # Initialize disjoint set data structure
+        parent_array = [-1] * total_points
+
+        # Connect border points
+        for i in range(points_per_side):
+            for j in range(points_per_side):
+                if (
+                    i == 0
+                    or j == 0
+                    or i == points_per_side - 1
+                    or j == points_per_side - 1
+                ):
+                    point = i * points_per_side + j
+                    parent_array[point] = 0
+
+        # Set the parent of the top-left corner to itself
+        parent_array[0] = -1
+        region_count = 1  # Start with one region (the border)
+
+        # Process each cell in the grid
+        for i in range(grid_size):
+            for j in range(grid_size):
+                # Treat each slash as an edge connecting two points
+                if grid[i][j] == "/":
+                    top_right = i * points_per_side + (j + 1)
+                    bottom_left = (i + 1) * points_per_side + j
+                    region_count += self._union(
+                        parent_array, top_right, bottom_left
+                    )
+                elif grid[i][j] == "\\":
+                    top_left = i * points_per_side + j
+                    bottom_right = (i + 1) * points_per_side + (j + 1)
+                    region_count += self._union(
+                        parent_array, top_left, bottom_right
+                    )
+
+        return region_count
+
+    def _find(self, parent_array: List[int], node: int) -> int:
+        if parent_array[node] == -1:
+            return node
+
+        parent_array[node] = self._find(parent_array, parent_array[node])
+        return parent_array[node]
+
+    def _union(self, parent_array: List[int], node1: int, node2: int) -> int:
+        parent1 = self._find(parent_array, node1)
+        parent2 = self._find(parent_array, node2)
+
+        if parent1 == parent2:
+            return 1  # Nodes are already in the same set, new region formed
+
+        parent_array[parent2] = parent1  # Union the sets
+        return 0  # No new region formed
