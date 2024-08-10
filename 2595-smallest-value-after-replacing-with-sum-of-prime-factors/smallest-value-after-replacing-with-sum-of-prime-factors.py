88635c316577104913ce7c23910dc3b3496485b6
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-10 21:08:25 +0530

    Time: 34 ms (85.50%) | Memory: 16.5 MB (88.55%) - LeetSync

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.py
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.py
@@ -1,17 +1,18 @@
 class Solution:
     def smallestValue(self, n: int) -> int:
-        sum = 0 
-        c = 2
-        org_n = n
-        while n > 1:
-            if(n % c == 0):
-                sum = sum + c
-                n = n // c
-            else:
-                c+=1
-        if sum == org_n:
-            return org_n
-        return self.smallestValue(sum)
-      
-        
-        
+        if n == 4: return 4
+        tab = [0, n]
+        while len(tab) > 1:
+            n = sum(tab)
+            newtab = []
+            i = 2
+            while i <= int(sqrt(n)):
+                if n % i == 0:
+                    n /= i
+                    newtab.append(i)
+                else:
+                    i += 1
+            tab = newtab
+            tab.append(n)
+        return int(tab[0])
+                    
