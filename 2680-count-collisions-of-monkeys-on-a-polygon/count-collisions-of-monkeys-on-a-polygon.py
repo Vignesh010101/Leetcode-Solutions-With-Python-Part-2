b847a46881265133281adc19a4e0dc10a0fe8370
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-11 22:03:43 +0530

    Time: 22 ms (99.55%) | Memory: 16.4 MB (73.09%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -1,11 +1,3 @@
 class Solution:
     def monkeyMove(self, n: int) -> int:
-        n=bin(n)[2:]
-        n=n[::-1] 
-        y,a=1,2
-        m=(10**9)+7
-        for i in n:
-            if i=='1':
-                y=(y*a)%m
-            a=(a*a)%m 
-        return (y-2)%m        
+        return (pow(2, n, 1000000007) - 2) % 1000000007
