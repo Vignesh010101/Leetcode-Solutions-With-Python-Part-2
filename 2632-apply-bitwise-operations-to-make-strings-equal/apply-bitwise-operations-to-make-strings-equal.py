1ef5c395ccc611938252a46f6ed7216608a5cff6
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-11 20:36:52 +0530

    Time: 27 ms (100.00%) | Memory: 17.6 MB (70.00%) - LeetSync

--- 2632-apply-bitwise-operations-to-make-strings-equal/apply-bitwise-operations-to-make-strings-equal.py
+++ 2632-apply-bitwise-operations-to-make-strings-equal/apply-bitwise-operations-to-make-strings-equal.py
@@ -1,13 +1,4 @@
 class Solution:
     def makeStringsEqual(self, s: str, target: str) -> bool:
-        target = list(set(target))
-        s = list(set(s))
-
-        if target == s: # 1st condition
-            return True
-        elif len(target) == len(s): # 2nd condition
-            return False
-        elif s == ['0'] or target == ['0']: # 3rd condition
-            return False
-        else:
+        if '1' in target and '1' in s or target==s:
             return True
