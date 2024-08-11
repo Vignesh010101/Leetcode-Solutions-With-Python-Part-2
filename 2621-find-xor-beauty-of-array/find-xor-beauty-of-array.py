9110e7e34ddf4f8403150539189058ad9852c9a2
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-11 16:38:44 +0530

    Time: 272 ms (38.25%) | Memory: 31.2 MB (24.42%) - LeetSync

--- 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.py
+++ 2621-find-xor-beauty-of-array/find-xor-beauty-of-array.py
@@ -1,6 +1,3 @@
 class Solution:
     def xorBeauty(self, nums: List[int]) -> int:
-        ans = 0
-        for i in nums:
-            ans ^= i
-        return ans
+        return reduce(xor, nums)
