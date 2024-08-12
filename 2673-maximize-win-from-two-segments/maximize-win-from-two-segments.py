ddc4fe92141df6dedde38892bae0e1d0fb7e93d6
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-12 21:47:38 +0530

    Time: 387 ms (100.00%) | Memory: 27.4 MB (49.02%) - LeetSync

--- 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.py
+++ 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.py
@@ -1,23 +1,15 @@
 class Solution:
-    def maximizeWin(self, prizePositions: List[int], k: int) -> int:
-        intervals = []
-        max_beffore = []
-        start_inx = 0
-        count = 0
-        for inx, pos in enumerate(prizePositions):
-            count += 1
-            while pos-k > prizePositions[start_inx]:
-                count -= 1
-                start_inx += 1
-            intervals.append((count, pos))
-            if not max_beffore or max_beffore[-1][0] < count:
-                max_beffore.append((count, pos))
-
-        max_solution = 0
-        while intervals:
-            count, pos = intervals.pop()
-            while max_beffore and max_beffore[-1][1] >= pos-k:
-                max_beffore.pop()
-            candidate = count + (0 if not max_beffore else max_beffore[-1][0])
-            max_solution = max(candidate, max_solution)
-        return max_solution
+    def maximizeWin(self, pos: List[int], k: int) -> int:
+        n = len(pos)
+        if k*2+1 >= pos[-1] - pos[0]: return n
+        pre = [0] * (n + 1)
+        ans = left = 0
+        for right, p in enumerate(pos):
+            while p - pos[left] > k:
+                left += 1
+            sz = right - left + 1
+            res = sz + pre[left]
+            if res > ans: ans = res
+            if sz >= pre[right]: pre[right+1] = sz
+            else: pre[right+1] = pre[right]
+        return ans
