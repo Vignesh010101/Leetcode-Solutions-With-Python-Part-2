b4db62f5c027f2198af616a91372dcce1c046e5f
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:31:58 +0530

    Time: 669 ms (62.64%) | Memory: 31.2 MB (47.25%) - LeetSync

--- 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
@@ -1,44 +1,11 @@
 class Solution:
-    
     def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
-        def rec(nums):
-            if len(nums)==0:
-                return 0
-            nums.sort()
-            psum=[nums[0]]
-            for i in range(1,len(nums)):
-                psum.append(psum[-1]+nums[i])
-            s=psum[-1]
-            ans=float("inf")
-            temp=0
-            n=len(nums)
-            for i in range(n):
-                ii=nums[i]
-                temp+=ii
-                s-=ii
-                a=abs(ii*(i+1)-temp)+abs(ii*(n-i-1)-s)
-                ans=min(ans,a)
-                # print(ii,ans,a)
-            return ans
-        # if len(arr)%k==0:
-        fans=0
-        vis=set()
-        for i in range(k):
-                temparr=[]
-                
-                j=i
-                while True:
-                    if j%len(arr) not in vis:
-                        vis.add(j%len(arr))
-                        temparr.append(arr[j%len(arr)])
-                    else:
-                        break
-                    j+=k
-                # print(temparr)
-                fans+=rec(temparr)
-                
-                    
-            
-        return fans
-                
-                    
+        gcd = math.gcd(len(arr), k)
+        
+        ans = 0
+        for i in range(gcd):
+            seq = arr[i::gcd]
+            m = int(statistics.median(seq))
+            for n in seq:
+                ans += abs(n-m)
+        return ans
