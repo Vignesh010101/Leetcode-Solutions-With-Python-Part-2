6d0481928f9e5830b9e92d116cb031761841b48b
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:21:28 +0530

    Time: 985 ms (14.68%) | Memory: 43.3 MB (75.99%) - LeetSync

--- 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -1,15 +1,32 @@
 class Solution:
     def minOperations(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()
-        aug = [(q, i) for i, q in enumerate(queries)]
-        aug.sort()
-        total = sum(nums)
-        prefix = 0
-        k = 0
-        ans = [0] * len(queries)
-        for q, i in aug:
-            while k < len(nums) and nums[k] < q:
-                prefix += nums[k]
-                k += 1
-            ans[i] = total - 2 * prefix + q * (2 * k - len(nums))
-        return ans       
+        prefix_sum = [nums[0]]
+        ans = []
+        for i in range(1, len(nums)):
+            prefix_sum.append(prefix_sum[-1] + nums[i])
+        
+        for target in queries:
+            l = 0
+            r = len(nums) - 1
+            pos = -2 # -2 for not found case
+            #left bin search             
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] > target:
+                    r = mid - 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    pos = mid
+                    r = mid - 1     
+
+            if pos == -2:
+                pos = min(l, r)
+            # pos == -1 leftmost case
+            if pos == -1:
+                ans.append(prefix_sum[-1] - (len(nums))*target)
+            else:
+                ans.append((pos+1)*target - prefix_sum[pos] + prefix_sum[-1] - prefix_sum[pos] - (len(nums) -1 - pos)*target)
+            
+        return ans
