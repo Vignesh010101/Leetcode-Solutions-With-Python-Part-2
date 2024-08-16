7ef0451ca1b07940bd84f61de88f0ce561095533
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:38:26 +0530

    Time: 71 ms (96.78%) | Memory: 59.2 MB (80.02%) - LeetSync

--- 2758-check-if-object-instance-of-class/check-if-object-instance-of-class.js
+++ 2758-check-if-object-instance-of-class/check-if-object-instance-of-class.js
@@ -4,21 +4,10 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function(obj, classFunction) {
-    while(obj!=null)
-    {
-        if(obj.constructor === classFunction)
-        {
-            return true;
-        }
-
-        obj = Object.getPrototypeOf(obj);
-
-    }
-
-    return false;
+    if(obj===null||obj===undefined || typeof classFunction !== 'function') return false;
+    return Object(obj) instanceof classFunction   
 };
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
  */
- 
