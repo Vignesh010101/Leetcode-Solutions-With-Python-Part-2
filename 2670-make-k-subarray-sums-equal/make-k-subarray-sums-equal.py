5c23ea20c7e36debfe52dee55e8d2c74b173fcd5
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:30:55 +0530

    Time: 712 ms (35.16%) | Memory: 31.9 MB (28.57%) - LeetSync

--- 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
@@ -1,19 +1,9 @@
 class Solution:
-    def makeSubKSumEqual(self,arr, k):
-        n = len(arr)
-        v = [[] for i in range(n + 1)]
-        k = gcd(n, k)
-        for i in range(n):
-            v[i % k].append(arr[i])
-        ans = 0
-        for i in range(k):
-            v[i].sort()
-            x = v[i][len(v[i]) // 2]
-            for j in v[i]:
-                ans += abs(x - j)
-        return ans
+    def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
 
-    def gcd(a, b):
-        if b == 0:
-            return a
-        return gcd(b, a % b)
+        g, d, ans = gcd(k,len(arr)), defaultdict(list), 0
+
+        for i, a in enumerate(arr): d[i%g].append(a)
+        for i in d: d[i].sort()
+            
+        return sum(d[i][-j-1]-d[i][j] for j in range((len(d[i]))//2) for i in d)
