d5ffe1113a8c14b555d18bd5977a76b20960790d
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:31:32 +0530

    Time: 1053 ms (5.49%) | Memory: 38 MB (6.59%) - LeetSync

--- 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
+++ 2670-make-k-subarray-sums-equal/make-k-subarray-sums-equal.py
@@ -1,9 +1,44 @@
 class Solution:
+    
     def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
-
-        g, d, ans = gcd(k,len(arr)), defaultdict(list), 0
-
-        for i, a in enumerate(arr): d[i%g].append(a)
-        for i in d: d[i].sort()
+        def rec(nums):
+            if len(nums)==0:
+                return 0
+            nums.sort()
+            psum=[nums[0]]
+            for i in range(1,len(nums)):
+                psum.append(psum[-1]+nums[i])
+            s=psum[-1]
+            ans=float("inf")
+            temp=0
+            n=len(nums)
+            for i in range(n):
+                ii=nums[i]
+                temp+=ii
+                s-=ii
+                a=abs(ii*(i+1)-temp)+abs(ii*(n-i-1)-s)
+                ans=min(ans,a)
+                # print(ii,ans,a)
+            return ans
+        # if len(arr)%k==0:
+        fans=0
+        vis=set()
+        for i in range(k):
+                temparr=[]
+                
+                j=i
+                while True:
+                    if j%len(arr) not in vis:
+                        vis.add(j%len(arr))
+                        temparr.append(arr[j%len(arr)])
+                    else:
+                        break
+                    j+=k
+                # print(temparr)
+                fans+=rec(temparr)
+                
+                    
             
-        return sum(d[i][-j-1]-d[i][j] for j in range((len(d[i]))//2) for i in d)
+        return fans
+                
+                    
