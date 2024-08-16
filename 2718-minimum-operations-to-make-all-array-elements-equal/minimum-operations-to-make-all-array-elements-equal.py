7fb37589055e86a814a78af4bcb2af2f6b7dc021
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-16 21:23:10 +0530

    Time: 698 ms (71.33%) | Memory: 43.8 MB (65.29%) - LeetSync

--- 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -1,25 +1,22 @@
 class Solution:
     def minOperations(self, nums: List[int], queries: List[int]) -> List[int]:
-        prefix = []
         nums.sort()
-        prev = 0
-        for num in nums:
-            prev += num
-            prefix.append(prev)
+        cumSum = [0] * len(nums)
+        res = []
+
+        cumSum[0] = nums[0]
+        for i in range(1, len(nums)):
+            cumSum[i] += cumSum[i-1] + nums[i]
         
-        data = []
-        for target in queries:
-            i = 0
-            k = len(prefix)
-            tot = 0
-            while i < k:
-                j = (i + k) // 2
-                if nums[j] > target: k = j
-                else: i = j + 1
+        for q in queries:
+            pos = bisect_right(nums, q)
+            if pos-1 >= 0 and nums[pos-1] == q:
+                pos = pos - 1
+            if pos == 0:
+                res.append(abs(cumSum[len(nums) - 1] - (q * len(nums))))
+            else:
+                s = abs((cumSum[pos - 1] - (q * pos)))
+                f = (cumSum[len(nums) - 1] - cumSum[pos - 1]) - (q * (len(nums) - pos))
+                res.append(s+f)
         
-            backs = prefix[i - 1] if i != 0 else 0
-            tot += i * target - backs
-            tot += (prefix[-1] - backs) - (len(nums) - i) * target
-            data.append(tot)
-        
-        return data
+        return res
