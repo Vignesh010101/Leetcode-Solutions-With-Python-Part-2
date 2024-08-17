0a8a80cbbd3bf5da6f2e1f0a5b12b10c07726ae3
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-17 22:40:49 +0530

    Time: 526 ms (10.99%) | Memory: 74.6 MB (16.48%) - LeetSync

--- 2778-frequency-tracker/frequency-tracker.py
+++ 2778-frequency-tracker/frequency-tracker.py
@@ -1,28 +1,24 @@
-from collections import defaultdict
-
 class FrequencyTracker:
-
     def __init__(self):
-        self.ds = defaultdict(int)
-        self.fs = defaultdict(int)
+        self.map = {} # Dictionary to store the count of each number
+        self.frequency = [0] * (10**5 + 1) # List to store the frequency of each count
 
-    def add(self, number: int) -> None:
-        self.fs[self.ds[number]] -= 1
-        self.ds[number] += 1
-        self.fs[self.ds[number]] += 1
+    def add(self, number):
+        freq = self.map.get(number, 0) # Get the current count of the number
+        if freq > 0 and self.frequency[freq] > 0: # If the current count is already present, decrement its frequency
+            self.frequency[freq] -= 1
+        self.map[number] = freq + 1 # Increment the count of the number in the map
+        self.frequency[self.map[number]] += 1 # Increment the frequency of the new count in the frequency list
 
-    def deleteOne(self, number: int) -> None:
-        self.fs[self.ds[number]] -= 1
-        self.ds[number] = max(0,self.ds[number] - 1)
-        self.fs[self.ds[number]] += 1
+    def deleteOne(self, n):
+        count = self.map.get(n, 0) # Get the current count of the number
+        if count > 0:
+            self.frequency[count] -= 1 # Decrement the frequency of the current count in the frequency list
+            if count == 1:
+                del self.map[n] # If the count is 1, remove the number from the map
+            else:
+                self.map[n] = count - 1 # Decrement the count of the number in the map
+                self.frequency[self.map[n]] += 1 # Increment the frequency of the new count in the frequency list
 
-    def hasFrequency(self, frequency: int) -> bool:
-        return self.fs[frequency] > 0
-        
-
-
-# Your FrequencyTracker object will be instantiated and called as such:
-# obj = FrequencyTracker()
-# obj.add(number)
-# obj.deleteOne(number)
-# param_3 = obj.hasFrequency(frequency)
+    def hasFrequency(self, number):
+        return number < len(self.frequency) and self.frequency[number] > 0 # Check if the frequency of the given number is greater than 0
