8439a29fe1b51debe7e8e3ae11319e5c0bd54c1a
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-17 21:52:46 +0530

    Time: 574 ms (68.98%) | Memory: 65 MB (37.23%) - LeetSync

--- 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
+++ 2677-cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.py
@@ -4,47 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-from collections import deque, defaultdict
+from collections import deque
 
 class Solution:
     def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root:
-            root.val = 0
-        else:
-            return None
-        que = deque()
-        que.append(root)
-        pat_tot = defaultdict(int)
-        while que:
-            node_arr = []
-            total_sum = 0
-            for _ in range(len(que)):
+        level_map = defaultdict(int)
+        level_map[-1] = root.val
+        q = deque([root])
+        level = -1
+        while len(q):
+            level += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    level_map[level] += node.left.val
+                    q.append(node.left)
 
-                node = que.popleft()
-                node_arr.append(node)
+                if node.right:
+                    level_map[level] += node.right.val
+                    q.append(node.right)
+
+        # level 0 and level 1 will always be zero
+        root.val = 0
+        level = -1
+        q = deque([root])
+        while len(q):
+            level += 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                node_child_sum = 0
+                if node.left:
+                    node_child_sum += node.left.val
+                if node.right:
+                    node_child_sum += node.right.val
 
                 if node.left:
-                    que.append(node.left)
-                    pat_tot[node]+=node.left.val
-                    total_sum+=node.left.val
-                
+                    node.left.val = level_map[level] - node_child_sum
+                    q.append(node.left)
                 if node.right:
-                    que.append(node.right)
-                    pat_tot[node]+=node.right.val
-                    total_sum+=node.right.val
-                
-            for node in node_arr:
-                if node.left:
-                    node.left.val = total_sum - pat_tot[node]
-                if node.right:
-                    node.right.val = total_sum - pat_tot[node]
-        return root
-            
-
-
-
-
-
-
-        
+                    node.right.val = level_map[level] - node_child_sum
+                    q.append(node.right)
+        return root   
