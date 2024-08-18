159d25e9c784f3a644de5904ac2b0e661b90d059
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-18 21:47:28 +0530

    Time: 608 ms (38.64%) | Memory: 34.9 MB (12.41%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.py
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.py
@@ -1,14 +1,12 @@
 class Solution:
     def matrixSum(self, nums: List[List[int]]) -> int:
-        total_sum = 0
-        
         for row in nums:
-            row.sort()
-        
-        for i in range(len(nums[0])):
-            maxi = 0
-            for j in range(len(nums)):
-                maxi = max(nums[j][i], maxi)
-            total_sum += maxi
-        
-        return total_sum
+            row.sort(reverse=True)
+        res = 0
+        n,m = len(nums),len(nums[0])
+        for i in range(m):
+            curr = -1
+            for j in range(n):
+                curr = max(curr,nums[j][i])
+            res += curr
+        return res
