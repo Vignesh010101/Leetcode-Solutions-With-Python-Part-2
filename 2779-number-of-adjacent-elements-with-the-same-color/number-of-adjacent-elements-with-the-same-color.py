9bca9679d8bdc5a890d1a15823486a915cdad80d
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-18 21:45:16 +0530

    Time: 1898 ms (27.52%) | Memory: 56.3 MB (65.77%) - LeetSync

--- 2779-number-of-adjacent-elements-with-the-same-color/number-of-adjacent-elements-with-the-same-color.py
+++ 2779-number-of-adjacent-elements-with-the-same-color/number-of-adjacent-elements-with-the-same-color.py
@@ -1,14 +1,22 @@
 class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
+        col = [0] * (n+1)  # to make sure ends are accounted for
+        ret = []
+        cur = 0
+        for i, c in queries:
+            if c != col[i]:
+                # removal:
+                if col[i] != 0:
+                    if col[i-1] == col[i]:
+                        cur -= 1
+                    if col[i+1] == col[i]:
+                        cur -= 1
+                # addition
+                if col[i-1] == c:
+                    cur += 1
+                if col[i+1] == c:
+                    cur += 1
+            ret.append(cur)
+            col[i] = c
+        return ret
 
-        ans, d, cnt = [], defaultdict(int), 0
-
-        for i, color in queries:
-
-            if d[i]: cnt-= (d[i-1] == d[i])+(d[i+1] == d[i])
-            d[i] = color
-            if d[i]: cnt+= (d[i-1] == d[i])+(d[i+1] == d[i])
-
-            ans.append(cnt)
-
-        return ans
