c3edb9d55217a246f4ff6a99ca3932efc15a5d45
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-20 13:29:49 +0530

    Time: 1630 ms (9.46%) | Memory: 39.3 MB (56.08%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.py
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.py
@@ -1,11 +1,14 @@
 class Solution:
     def matrixSumQueries(self, n: int, queries: List[List[int]]) -> int:
-        rows, cols = set(i for i in range(n)), set(i for i in range(n))
-        result = 0
-        action_for_query = {0: (rows, cols), 1: (cols, rows)}
-        for query_type, index, value in reversed(queries):
-            modified_axis, opposite_axis = action_for_query[query_type]
-            if index in modified_axis:
-                modified_axis.remove(index)
-                result += len(opposite_axis) * value
-        return result
+
+        rowcol, ans, Seen = [n,n], 0, [0,0]
+
+        for typ, idx, val in reversed(queries):
+     
+            if Seen[typ]&(1<<idx): continue
+            Seen[typ]|= (1<<idx)
+    
+            ans+= val * rowcol[typ]
+            rowcol[typ^1]-= 1
+
+        return  ans
