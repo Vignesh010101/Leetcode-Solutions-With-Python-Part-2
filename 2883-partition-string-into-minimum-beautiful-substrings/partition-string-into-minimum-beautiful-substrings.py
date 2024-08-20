bced8b232f044150ad4a73211dc05dc7b36209f3
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-20 13:59:23 +0530

    Time: 40 ms (84.15%) | Memory: 16.7 MB (28.96%) - LeetSync

---
+++ 2883-partition-string-into-minimum-beautiful-substrings/partition-string-into-minimum-beautiful-substrings.py
@@ -0,0 +1,15 @@
+powers = tuple(bin(pow(5,i))[2:] for i in range(7))
+
+class Solution:
+    def minimumBeautifulSubstrings(self, s: str) -> int:
+
+        def dp(s):
+            if s in powers: return 1 
+            if not s: return inf
+            
+            return 1+ min((dp(s[len(p):]) for p in powers
+                            if s.startswith(p)), default = inf)
+
+        ans = dp(s)
+
+        return ans if ans != inf else -1
