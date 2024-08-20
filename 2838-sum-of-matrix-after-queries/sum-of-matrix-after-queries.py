250be7450d65c5942a5a940cc57af11a296d2495
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-20 13:27:35 +0530

    Time: 1657 ms (6.76%) | Memory: 39.1 MB (75.00%) - LeetSync

--- 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.py
+++ 2838-sum-of-matrix-after-queries/sum-of-matrix-after-queries.py
@@ -1,18 +1,13 @@
 class Solution:
-    def matrixSumQueries(self, n: int, queries: List[List[int]]) -> int:
-        ans = 0 
-        rowchng, colchng = 0,0
-        row, col = set(), set()
-        for i in range(len(queries)-1,-1,-1):
-            typ, ind, val = queries[i]
-            if typ == 0:
-                if ind not in row:
-                    row.add(ind)
-                    ans += (n-colchng)*val
-                    rowchng += 1
-            else:
-                if ind not in col:
-                    ans += (n-rowchng)*val
-                    colchng += 1
-                    col.add(ind)
+    def matrixSumQueries(self, n: int, queries: list) -> int:
+        rowcol = [n, n]
+        ans = 0
+        seen = [0, 0]
+
+        for t, i, v in reversed(queries):
+            if not seen[t] & (1 << i):
+                seen[t] |= 1 << i
+                ans += v * rowcol[t]
+                rowcol[t ^ 1] -= 1
+
         return ans
