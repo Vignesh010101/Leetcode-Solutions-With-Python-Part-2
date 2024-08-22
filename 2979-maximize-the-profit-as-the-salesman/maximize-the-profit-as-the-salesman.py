3f1e763b5b6c28f6f97d47a886830731b0281f9f
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-22 22:37:25 +0530

    Time: 1144 ms (34.25%) | Memory: 44.7 MB (62.98%) - LeetSync

--- 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.py
+++ 2979-maximize-the-profit-as-the-salesman/maximize-the-profit-as-the-salesman.py
@@ -1,19 +1,13 @@
 class Solution:
-    def maximizeTheProfit(self, n: int, offers: List[List[int]]) -> int:
-        mp = {}
-        for i in offers:
-            if i[0] not in mp:
-                mp[i[0]] = []
-            mp[i[0]].append([i[1], i[2]])
-        
-        arr = [0] * n
-        prof = 0
-        
-        for i in range(n):
-            if i in mp:
-                for j in mp[i]:
-                    arr[j[0]] = max(arr[j[0]], j[1] + prof)
-            prof = max(prof, arr[i])
-        
-        return prof
-        
+  def maximizeTheProfit(self, n: int, arr: List[List[int]]) -> int:
+    res, pos = [0] * (n + 1), 0
+    arr.sort(key=lambda x: (x[1], x[0]))
+
+    for i in range(1, n + 1):
+      best_val = res[i - 1]
+      while pos < len(arr) and arr[pos][1] == i - 1:
+        best_val = max(best_val, arr[pos][2] + res[arr[pos][0]])
+        pos += 1
+      res[i] = best_val
+    
+    return res[-1]
