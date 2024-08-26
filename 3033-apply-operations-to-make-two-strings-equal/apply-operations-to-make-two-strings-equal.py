81928780fa8bab9e6076e86141bc8f3ed47b05a6
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-26 22:41:16 +0530

    Time: 51 ms (53.33%) | Memory: 17.3 MB (55.00%) - LeetSync

--- 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.py
+++ 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.py
@@ -1,18 +1,20 @@
 class Solution:
     def minOperations(self, s1: str, s2: str, x: int) -> int:
-        diff = []
-        ret = 0
+        
+        ones = []
         for i in range(len(s1)):
             if s1[i] != s2[i]:
-                diff.append(i)
-        if len(diff) & 1: return -1
+                ones.append(i)
         
         @cache
-        def helper(l, r):
-            if l > r: return 0
-            ret = min(diff[l+1]-diff[l] + helper(l+2, r), diff[r]-diff[r-1] + helper(l, r-2), x + helper(l+1, r-1))
+        def dp(i, cheat):
+            if i == len(ones):
+                return 0 if not cheat else inf
+            ret = dp(i+1, not cheat) + x / 2
+            if i < len(ones)-1:
+                pos_a, pos_b = ones[i], ones[i+1]
+                ret = min(ret, dp(i+2, cheat)+pos_b-pos_a)
             return ret
-        return helper(0, len(diff)-1)
         
-        
-        
+        ret = dp(0, False)
+        return int(ret) if ret != inf else -1
