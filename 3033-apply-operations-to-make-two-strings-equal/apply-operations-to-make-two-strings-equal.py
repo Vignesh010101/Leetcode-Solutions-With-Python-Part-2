f03baca17d758ead8c4709c035bad2d9ad37abcb
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-08-26 22:40:04 +0530

    Time: 53 ms (50.00%) | Memory: 17.2 MB (55.00%) - LeetSync

--- 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.py
+++ 3033-apply-operations-to-make-two-strings-equal/apply-operations-to-make-two-strings-equal.py
@@ -1,12 +1,5 @@
 class Solution:
     def minOperations(self, s1: str, s2: str, x: int) -> int:
-        # s1 = "1100011000", 
-        # s2 = "0101001010"
-        # 0,3,5,8
-        
-        # s1 = "1100000000", 
-        # s2 = "0101001010"
-        # 0,3,6,8 (6-3)
         
         diff =[]
         n = len(s1)
