71bab7b31f4c62b07df1131e95122709a3f14184
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-11-18 19:16:43 +0530

    Time: 2143 ms (5.34%) | Memory: 20.2 MB (41.75%) - LeetSync

--- 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.py
+++ 3556-final-array-state-after-k-multiplication-operations-ii/final-array-state-after-k-multiplication-operations-ii.py
@@ -1,23 +1,26 @@
+from heapq import *
 class Solution:
-    def getFinalState(self, nums: List[int], k: int, 
-                multiplier: int, mod = 1_000_000_007) -> List[int]:
-
-        if multiplier == 1: return nums                     # <- 1)
-
-        n = len(nums)
-        unseen, ans = set(range(n)), [0] * n
-
-        heapify(heap:= list(zip(nums, range(len(nums)))))   # <- 2)
-
-        while k > 0 and unseen:                             # <- 3)
-            num, idx = heap[0]                              
-            heappushpop(heap, (num * multiplier, idx))
-            k-= 1
-            unseen.discard(idx)
-
-        for i in range(n):                                  # <- 4)
-            num, idx = heappop(heap)
-            ans[idx] = (num * pow(multiplier,
-                              k//n + (i < k%n), mod)) % mod
-
-        return ans                                          # <- 5)
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        if multiplier==1: return nums
+        mod=1_000_000_007
+        if len(nums)==1:
+            nums[0]=(nums[0]*pow(multiplier,k,mod))%mod
+            return nums
+        while True and k>0:
+            mn=min(nums)
+            mx=max(nums)
+            if mn*multiplier>mx: break
+            for i in range(len(nums)):
+                if k>0 and nums[i]==mn:
+                    k-=1
+                    nums[i]*=multiplier
+        nums=[[nums[i],i] for i in range(len(nums))]
+        nums.sort()
+        val1,val2=k//len(nums),k//len(nums)+1
+        remaining=k%len(nums)
+        for i in range(remaining):
+            nums[i][0]=(nums[i][0]*pow(multiplier,val2,mod))%mod
+        for i in range(remaining,len(nums)):
+            nums[i][0]=(nums[i][0]*pow(multiplier,val1,mod))%mod
+        nums.sort(key=lambda x:x[1])
+        return [i[0] for i in nums]
