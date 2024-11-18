b2f7fc1ced9b2491fc77d2eb8875cf3f32f6f869
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-11-18 18:23:49 +0530

    Time: 4 ms (87.50%) | Memory: 16.9 MB (95.88%) - LeetSync

--- 3536-find-the-count-of-monotonic-pairs-ii/find-the-count-of-monotonic-pairs-ii.py
+++ 3536-find-the-count-of-monotonic-pairs-ii/find-the-count-of-monotonic-pairs-ii.py
@@ -1,13 +1,17 @@
 class Solution:
     def countOfPairs(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [[0]*1001 for _ in range(n+1)]
-        dp[n] = [1]*1001
-        for i in range(n-1, -1, -1): 
-            diff = 0 
-            if i: diff = max(0, nums[i] - nums[i-1])
-            for j in range(1000, -1, -1): 
-                if j+1 <= 1000: dp[i][j] = dp[i][j+1]
-                if j+diff <= nums[i]: 
-                    dp[i][j] = (dp[i][j] + dp[i+1][j+diff]) % 1_000_000_007
-        return dp[0][0]
+
+        n, mn = len(nums), nums.pop(0)
+        num1, num2 = 0, mn
+    
+        for num in nums:
+            if num < num1: return 0
+
+            if num > num1 + num2: 
+                num1 = num - num2  
+
+            num2 = num - num1
+
+            if num2 < mn: mn = num2
+        
+        return comb(n + mn, mn) % 1_000_000_007
