9ba50d161bdbc655c6f2f550735d1e420baa6d80
Vignesh <68474341+Vignesh010101@users.noreply.github.com>
2024-11-18 19:23:53 +0530

    Time: 15 ms (88.61%) | Memory: 17.1 MB (64.15%) - LeetSync

--- 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.py
+++ 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.py
@@ -1,18 +1,29 @@
 class Solution:
-    def maxScore(self, grid: List[List[int]]) -> int: 
-        H = [set([]) for i in range(101)]
-        n = len(grid)
-        m = len(grid[0])
-        for i in range(n):
-            for j in range(m):
-                H[grid[i][j]].add(2**i)
-        d = [0 for i in range(2**10)]
-        for i in range(101):
-            new_d = [x for x in d]
-            for x in range(2**10):
-                for y in H[i]:
-                    if y|x != x:
-                        x2 = y|x
-                        new_d[x2] = max(new_d[x2], i+d[x])
-            d = new_d
-        return max(d)
+    def maxScore(self, grid: List[List[int]]) -> int:
+
+        @lru_cache(None)
+        def dp(i: int, mask: int)-> int:
+
+            if i == len(values): return 0
+            cur, value = mask, values[i]
+            
+            bits, j, res = d[value], 0, 0
+            
+            for _ in range(bits.bit_length()):
+
+                if (bits%2,cur%2) == (1, 0):
+                    res = max(res, value + dp(i + 1, mask | (1 << j)))
+                j, bits, cur = j + 1, bits//2, cur//2
+                
+            if not res: res = dp(i + 1, mask)
+
+            return res
+        
+
+        d = defaultdict(int)
+
+        for idx, row in enumerate(grid):
+            for num in row: d[num]|= (1<<idx)
+
+        values = sorted(d, reverse = True)
+        return dp(0, 0)
